72c1ba5ec4cbef1f9ed454311a700aa200847061
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-02-27 15:43:25 +0530

    Update leave.js

--- src/Commands/Dev/leave.js
+++ src/Commands/Dev/leave.js
@@ -7,6 +7,14 @@ module.exports = {
     react: "✅",
     description: 'Leave the current group',
     async execute(client, arg, M) {
+        const commandName = this.name || this.aliases[0];
+        const disabledCommands = await client.DB.get(`disabledCommands`);
+        const isDisabled = disabledCommands && disabledCommands.some(disabledCmd => disabledCmd.name === commandName);
+        
+        if (isDisabled) {
+            const disabledCommand = disabledCommands.find(cmd => cmd.name === commandName);
+            return M.reply(`This command is disabled for the reason: *${disabledCommand.reason}*`);
+        } 
         try {
             client.groupLeave(M.from)
                 .then(() => M.reply('✅ Successfully left the group!'))
