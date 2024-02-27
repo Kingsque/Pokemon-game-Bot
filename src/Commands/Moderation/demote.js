37a38fe7cb17255152af50fc9404c85c7df583b5
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-02-27 15:50:02 +0530

    Update demote.js

--- src/Commands/Moderation/demote.js
+++ src/Commands/Moderation/demote.js
@@ -1,20 +1,37 @@
+const ms = require('parse-ms');
+
 module.exports = {
     name: 'demote',
     aliases: ['demo'],
     exp: 5,
+    cool: 4,
     react: "✅",
     category: 'moderation',
-    description: 'Demotes the taged user',
+    description: 'Demotes the tagged user',
     async execute(client, arg, M) {
+        const commandName = this.name || this.aliases[0];
+        const disabledCommands = await client.DB.get(`disabledCommands`);
+        const isDisabled = disabledCommands && disabledCommands.some(disabledCmd => disabledCmd.name === commandName);
+        
+        if (isDisabled) {
+            const disabledCommand = disabledCommands.find(cmd => cmd.name === commandName);
+            return M.reply(`This command is disabled for the reason: *${disabledCommand.reason}*`);
+        } 
+        const cooldownMs = this.cool * 1000;
+        const lastSlot = await client.DB.get(`${M.sender}.demote`);
+
+        if (lastSlot !== null && cooldownMs - (Date.now() - lastSlot) > 0) {
+            const remainingCooldown = ms(cooldownMs - (Date.now() - lastSlot), { long: true });
+            return M.reply(`*You have to wait ${remainingCooldown} for another slot*`);
+        }
         if (!M.mentions.length) return M.reply('You must tag the user before using!')
         const groupMetadata = await client.groupMetadata(M.from)
         const groupMembers = groupMetadata?.participants || []
         const groupAdmins = groupMembers.filter((v) => v.admin).map((v) => v.id)
-        let adminUsers = []
-        // Filter the users who are admin and push the not admin users in the users var
-        M.mentions.filter((users) => (groupAdmins.includes(users) ? adminUsers.push(users) : null))
-        await client.groupParticipantsUpdate(M.from, adminUsers, 'demote').then((res) => {
-            M.reply(`Done! Demoting ${adminUsers.length} users`)
+        let nonAdminUsers = M.mentions.filter((user) => !groupAdmins.includes(user))
+        await client.groupParticipantsUpdate(M.from, nonAdminUsers, 'demote').then((res) => {
+            M.reply(`Done! Demoting ${nonAdminUsers.length} users`)
         })
+        await client.DB.set(`${M.sender}.demote`, Date.now());
     }
 }
