ee87269053ff21f2d0386accc5a01790e4db7974
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-02-27 07:07:18 +0000

    card shop directory

--- src/Commands/Cshop/buy-card.js
+++ src/Commands/Cshop/buy-card.js
@@ -1,77 +1,67 @@
-module.exports = {
-  name: "buy-card",
-  aliases: ["b-c"],
-  exp: 0,
-  react: "✅",
-  category: "card shop",
-  async execute(client, arg, M) {
-  const auction = (await client.DB.get('cshop')) || [];
-    if (!auction.includes(M.from)) return M.reply(`join auction gc by using ${client.prefix}support`);
-    
-    let dime = await client.rpg.get(`${M.sender}.diamond`) || 0;
+const ms = require('parse-ms');
 
-    if (arg === "1") {
-      if (dime < 50000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Madara-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 50000);
-      M.reply("You have succesfully purchased madara. it's in your collection");
-    } else if (arg === "2") {
-      if (dime < 40000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Goku-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 40000);
-      M.reply("You have succesfully purchased goku. it's in your collection");
-    } else if (arg === "3") {
-      if (dime < 45000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Yuji Itadori and Sukuna-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 45000);
-      M.reply("You have succesfully purchased yuji and sukuna. it's in your collection");
-    } else if (arg === "4") {
-      if (dime < 60000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Tanjiro Kamado-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 60000);
-      M.reply("You have succesfully purchased tanjiro. it's in your collection");
-    } else if (arg === "5") {
-      if (dime < 40000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Genos-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 40000);
-      M.reply("You have succesfully purchased genos. it's in your collection");
-    } else if (arg === "6") {
-      if (dime < 55000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Allen Walker-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 55000);
-      M.reply("You have succesfully purchased Allen walker. it's in your collection");
-    } else if (arg === "7") {
-      if (dime < 65000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Yae Miko-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 65000);
-      M.reply("You have succesfully purchased yae miko. it's in your collection");
-    } else if (arg === "8") {
-      if (dime < 50000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Broly-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 50000);
-      M.reply("You have succesfully purchased broly. it's in your collection");
-    } else if (arg === "9") {
-      if (dime < 60000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Hayase Nagatoro-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 60000);
-      M.reply("You have succesfully purchased nagataro. it's in your collection");
-    } else if (arg === "10") {
-      if (dime < 70000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Collection`, `Ace x Sabo x Luffy-6`);
-      await client.rpg.sub(`${M.sender}.diamond`, 70000);
-      M.reply("You have succesfully purchased Ace x Sabo x Luffy. it's in your collection");
-    } else if (arg === "11") {
-      if (dime < 10000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Deck`, `Denji's dream life-S`);
-      await client.rpg.sub(`${M.sender}.diamond`, 100000);
-      M.reply("You have succesfully purchased denji. it's in your collection");
-    } else if (arg === "12") {
-      if (dime < 120000) return M.reply('you dont have enough diamonds');
-      await client.DB.push(`${M.sender}_Deck`, `Love & Mist Hashira vs Upper Moon-S`);
-      await client.rpg.sub(`${M.sender}.diamond`, 120000);
-      M.reply("You have succesfully purchased Love & Mist Hashira vs Upper Moon. it's in your collection");
-    } else {
-      M.reply("you didn't provide a card number");
+module.exports = {
+    name: "buy-card",
+    aliases: ["b-c"],
+    exp: 0,
+    cool: 4,
+    react: "✅",
+    category: "card shop",
+    description: 'To buy cards from card shop',
+    async execute(client, arg, M) {
+        const commandName = this.name || this.aliases[0];
+        const disabledCommands = await client.DB.get(`disabledCommands`);
+        const isDisabled = disabledCommands && disabledCommands.some(disabledCmd => disabledCmd.name === commandName);
+        
+        if (isDisabled) {
+            const disabledCommand = disabledCommands.find(cmd => cmd.name === commandName);
+            return M.reply(`This command is disabled for the reason: *${disabledCommand.reason}*`);
+        } 
+        const cooldownMs = this.cool * 1000;
+        const lastSlot = await client.DB.get(`${M.sender}.buyc`);
+
+        if (lastSlot !== null && cooldownMs - (Date.now() - lastSlot) > 0) {
+            const remainingCooldown = ms(cooldownMs - (Date.now() - lastSlot), { long: true });
+            return M.reply(`*You have to wait ${remainingCooldown} for another slot*`);
+        }
+        const auction = (await client.DB.get('cshop')) || [];
+        if (!auction.includes(M.from)) return M.reply(`Join the official group by using ${client.prefix}support, every saturday card shop commands are turned on`);
+        
+        let dime = await client.rpg.get(`${M.sender}.diamond`) || 0;
+
+        const cardPrices = {
+            "1": 50000,
+            "2": 40000,
+            "3": 45000,
+            "4": 60000,
+            "5": 40000,
+            "6": 55000,
+            "7": 65000,
+            "8": 50000,
+            "9": 45000,
+            "10": 70000
+        };
+
+        const cardNames = {
+            "1": "Madara",
+            "2": "Goku",
+            "3": "Yuji Itadori and Sukuna",
+            "4": "Tanjiro",
+            "5": "Genos",
+            "6": "Allen Walker",
+            "7": "Yae Miko",
+            "8": "Broly",
+            "9": "Hayase Nagatoro",
+            "10": "Ace x Sabo x Luffy"
+        };
+
+        if (!cardPrices[arg]) return M.reply("Invalid card number.");
+
+        if (dime < cardPrices[arg]) return M.reply("You don't have enough diamonds.");
+
+        await client.DB.push(`${M.sender}_Collection`, `${cardNames[arg]}-6`);
+        await client.rpg.sub(`${M.sender}.diamond`, cardPrices[arg]);
+        M.reply(`You have successfully purchased ${cardNames[arg]}. It's in your collection.`);
+        await client.DB.set(`${M.sender}.buyc`, Date.now());
     }
-  },
 };
--- src/Commands/Cshop/cshop.js
+++ src/Commands/Cshop/cshop.js
@@ -1,18 +1,35 @@
+const ms = require('parse-ms');
+
 module.exports = {
     name: 'card-shop',
     aliases: ['cshop'],
     category: 'card shop',
     exp: 5,
+    cool: 4,
     react: "✅",
-    description: 'shows card shop items',
+    description: 'Shows available card shop items',
     async execute(client, arg, M) {
+        const commandName = this.name || this.aliases[0];
+        const disabledCommands = await client.DB.get(`disabledCommands`);
+        const isDisabled = disabledCommands && disabledCommands.some(disabledCmd => disabledCmd.name === commandName);
         
-      //  const cshop = (await client.DB.get('cshop')) || []
-        
-      //  if(!cshop.includes(M.from)) return M.reply("join Auction and cards group for it by using :support")
-     //Card Game enable checker
+        if (isDisabled) {
+            const disabledCommand = disabledCommands.find(cmd => cmd.name === commandName);
+            return M.reply(`This command is disabled for the reason: *${disabledCommand.reason}*`);
+        } 
+        const cooldownMs = this.cool * 1000;
+        const lastSlot = await client.DB.get(`${M.sender}.cshop`);
 
-        let shop =  `⛺ *|------< CARD SHOP >-------|* ⛺\n\n🎉 *welcome to our card shop.Here are the list of cards* 🎉\n\n*#1*\n🔥 *Name:* Madara\n🔩Tier: 6\n💰 *Price:* 50000 diamonds\n🛠️ *Source:* Naruto\n\n*#2*\n🔥 *Name:* Goku\n🔩Tier: 6\n💰 *Price:* 40000 diamonds\n🛠️ *Source:* Dragon Ball\n\n*#3*\n🔥 *Name:* Yuji Itadori and Sukuna\n🔩Tier: 6\n💰 *Price:* 45000 diamonds\n🛠️ *Source:* Jujutsu Kaisen\n\n*#4*\n🔥 *Name:* Tanjiro\n🔩Tier: 6\n💰 *Price:* 60000 diamonds\n🛠️ *Source:* Demon slayer\n\n*#5*\n🔥 *Name:* Genos\n🔩Tier: 6\n💰 *Price:* 40000 diamonds\n🛠️ *Source:* One Punch Man\n\n*#6*\n\n🔥 *Name:* Allen Walker\n🔩Tier: 6\n💰 *Price:* 55000 diamonds\n\n🛠️ *Source:* D.gray Man\n\n*#7*\n🔥 *Name:* Yae Miko\n🔩Tier: 6\n💰 *Price:* 65000 diamonds\n🛠️ *Source:* genshin impact\n\n*#8*\n🔥 *Name: Broly* \n🔩Tier: 6\n💰 *Price:* 50000 diamonds\n🛠️ *Source:* Drago Ball\n\n*#9*\n🔥 *Name:* Hayase Nagatoro\n🔩Tier: 6\n💰 *Price:*  45000 diamonds\n🛠️ *Source:* i dot know\n\n*#10*\n🔥 *Name:* Ace x Sabo x Luffy\n🔩Tier: 6\n💰 *Price:* 70000 diamonds\n🛠️ *Source:* One Piece\n\n*#11*\n🔥 *Name:* Denji dream\n🔩Tier: S\n💰 *Price:* 100000 diamonds\n🛠️ *Source:* Chainsaw Man\n\n*#12*\n🔥 *Name:* Love & Mist Hashira vs Upper Moons\n🔩Tier: S\n💰 *Price:* 120000 diamonds\n🛠️ *Source:* Demon Slayer\n\n🔰 *Note:* *Use :buy-card <Index_Number> to select your card.*\nEvery week This list will be updated`
-        M.reply(shop)  
+        if (lastSlot !== null && cooldownMs - (Date.now() - lastSlot) > 0) {
+            const remainingCooldown = ms(cooldownMs - (Date.now() - lastSlot), { long: true });
+            return M.reply(`*You have to wait ${remainingCooldown} for another slot*`);
+        }
+
+        const auction = (await client.DB.get('cshop')) || [];
+        if (!auction.includes(M.from)) return M.reply(`Join the official group by using ${client.prefix}support, every weekend card shop commands are turned on`);
+        
+        let shop =  `⛺ *|------< CARD SHOP >-------|* ⛺\n\n🎉 *Welcome to our card shop. Here are the list of available cards:* 🎉\n\n*#1*\n🔥 *Name:* Madara\n🔩Tier: 6\n💰 *Price:* 50000 diamonds\n🛠️ *Source:* Naruto\n\n*#2*\n🔥 *Name:* Goku\n🔩Tier: 6\n💰 *Price:* 40000 diamonds\n🛠️ *Source:* Dragon Ball\n\n*#3*\n🔥 *Name:* Yuji Itadori and Sukuna\n🔩Tier: 6\n💰 *Price:* 45000 diamonds\n🛠️ *Source:* Jujutsu Kaisen\n\n*#4*\n🔥 *Name:* Tanjiro\n🔩Tier: 6\n💰 *Price:* 60000 diamonds\n🛠️ *Source:* Demon Slayer\n\n*#5*\n🔥 *Name:* Genos\n🔩Tier: 6\n💰 *Price:* 40000 diamonds\n🛠️ *Source:* One Punch Man\n\n*#6*\n🔥 *Name:* Allen Walker\n🔩Tier: 6\n💰 *Price:* 55000 diamonds\n🛠️ *Source:* D.gray Man\n\n*#7*\n🔥 *Name:* Yae Miko\n🔩Tier: 6\n💰 *Price:* 65000 diamonds\n🛠️ *Source:* Genshin Impact\n\n*#8*\n🔥 *Name:* Broly\n🔩Tier: 6\n💰 *Price:* 50000 diamonds\n🛠️ *Source:* Dragon Ball\n\n*#9*\n🔥 *Name:* Hayase Nagatoro\n🔩Tier: 6\n💰 *Price:*  45000 diamonds\n🛠️ *Source:* Unknown\n\n*#10*\n🔥 *Name:* Ace x Sabo x Luffy\n🔩Tier: 6\n💰 *Price:* 70000 diamonds\n🛠️ *Source:* One Piece\n\n🔰 *Note:* *Use :buy-card <Index_Number> to select your card.*\nEvery week, this list will be updated.`;
+        M.reply(shop); 
+        await client.DB.set(`${M.sender}.slot`, Date.now());
     }
-            }
+};
