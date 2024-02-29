71449138ef0a5b19d9f5780ee2adb0b49981ba1d
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-02-29 12:18:52 +0530

    Create mode.js

---
+++ src/Commands/Dev/mode.js
@@ -0,0 +1,39 @@
+module.exports = {
+    name: 'mode',
+    aliases: ['mode'],
+    category: 'dev',
+    exp: 5,
+    cool: 4,
+    react: "✅",
+    description: 'Change the mode of the bot (self, private, public)',
+    async execute(client, arg, M) {
+        const validModes = ['self', 'private', 'public'];
+        const requestedMode = arg.toLowerCase().trim();
+        
+        if (!validModes.includes(requestedMode)) {
+            return M.reply("Invalid mode! Please choose either 'self', 'private', or 'public'.");
+        }
+
+        const mode = await client.DB.get(`mode`);
+
+        if (requestedMode === 'self') {
+            if (mode === 'self') {
+                return M.reply('The mode is already self');
+            }
+            await client.DB.set(`mode`, 'self');
+            return M.reply('Now only host of this bot can use command');
+        } else if (requestedMode === 'private') {
+            if (mode === 'private') {
+                return M.reply('The mode is already private');
+            }
+            await client.DB.set(`mode`, 'private');
+            return M.reply('Now only mods of this bot can use command');
+        } else if (requestedMode === 'public') {
+            if (mode === 'public') {
+                return M.reply('The mode is already public');
+            }
+            await client.DB.set(`mode`, 'public');
+            return M.reply('Now everyone on this this bot can use command');
+        }
+    }
+};
