dcd86dfa38b926438ffa12021af41ee53595021e
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-01 20:48:03 +0530

    Update hi.js

--- src/Commands/General/hi.js
+++ src/Commands/General/hi.js
@@ -16,12 +16,12 @@ module.exports = {
             return M.reply(`This command is disabled for the reason: *${disabledCommand.reason}*`);
         } 
         
+        const now = Date.now(); // Get current timestamp
         const cooldownSeconds = this.cool;
         const lastSlot = await client.DB.get(`${M.sender}.${commandName}`);
-        const now = Math.floor(Date.now() / 1000);
-
-        if (lastSlot !== null && now - lastSlot < cooldownSeconds) {
-            const remainingCooldown = cooldownSeconds - (now - lastSlot);
+      
+        if (lastSlot !== null && now - lastSlot < cooldownSeconds * 1000) {
+            const remainingCooldown = Math.ceil((cooldownSeconds * 1000 - (now - lastSlot)) / 1000);
             return M.reply(`*You have to wait ${remainingCooldown} seconds for another slot*`);
         }
 
@@ -29,9 +29,9 @@ module.exports = {
         const hi = hello[Math.floor(Math.random() * hello.length)];
         
         const contact = await client.contact.getContact(M.sender, client);
-        const username = contact ? contact.username : 'there';
+        const username = contact && contact.username ? contact.username : 'there';
         
-        await client.DB.set(`${M.sender}.${commandName}`, now); // Update cooldown timestamp
+        await client.DB.set(`${M.sender}.${commandName}`,  now); // Update cooldown timestamp
         M.reply(`${hi} ${username}. How are you today?`);
     }
 }
