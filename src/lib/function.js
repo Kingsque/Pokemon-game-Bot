10e752e5a1d1ede68c7762843516c0fb08c324b8
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-12 15:12:33 +0530

    Update function.js

--- src/lib/function.js
+++ src/lib/function.js
@@ -309,7 +309,7 @@ const term = (param) =>
     })
 
 const restart = () => {
-    exec('pm2 start src/haven.js', (err, stdout, stderr) => {
+    exec('pm2 start src/aurora.js', (err, stdout, stderr) => {
         if (err) {
             console.log(err)
             return
@@ -377,5 +377,6 @@ module.exports = {
     getFormattedUrl,
     search,
     convertMs,
+    extractUrls,
     gpt
 }
