0b082aec187fd0d7b7cea6f1a8bfc2195d16ae6e
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-21 20:51:05 +0530

    Update view.js

--- src/Commands/Cards/view.js
+++ src/Commands/Cards/view.js
@@ -9,23 +9,28 @@ module.exports = {
   react: "✅",
   category: "card game",
   description: "Views any card from the bot",
-  async execute(client, arg, M) {
-    const cardName = arg.trim(); // Accept card name as input
+  async execute(client, args, M) {
+    const [cardName, cardTier] = args.trim().split(","); // Accept card name and tier as input
 
     const filePath = path.join(__dirname, '../../Helpers/card.json');
     const data = require(filePath);
-    const cardData = data.find((cardData) => cardData.title === cardName); // Search by card name
+    
+    let cardData;
+    if (cardTier) {
+      cardData = data.find((cardData) => cardData.title === cardName && cardData.tier === cardTier); // Search by card name and tier
+    } else {
+      cardData = data.find((cardData) => cardData.title === cardName); // Search by card name only
+    }
 
     if (!cardData) {
       return M.reply("❗ Card data not found.");
     }
 
-    const cardTier = cardData.tier;
     const cardUrl = cardData.url;
     const imageUrl = cardUrl;
     const isGif = imageUrl.endsWith('.gif');
     const file = await client.utils.getBuffer(imageUrl);
-    const text = `💎 Card Details 💎\n\n🌊 Name: ${cardName}\n\n🌟 Tier: ${cardTier}\n\n${cardData.description}`;
+    const text = `💎 Card Details 💎\n\n🌊 Name: ${cardName}\n\n🌟 Tier: ${cardData.tier}\n\n${cardData.description}`;
 
     if (isGif) {
       const giffed = await client.utils.gifToMp4(file);
