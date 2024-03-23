201028571bde77c7c887e3789aabfd9333d5cd5d
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-23 15:20:19 +0530

    Update ebg.js

--- src/Commands/Event/ebg.js
+++ src/Commands/Event/ebg.js
@@ -1,3 +1,9 @@
+const axios = require('axios');
+const { createCanvas, loadImage } = require('canvas');
+const fs = require('fs');
+const path = require('path');
+const { createDeflate } = require('zlib');
+
 module.exports = {
   name: 'ebackground',
   aliases: ['ebgs'],
