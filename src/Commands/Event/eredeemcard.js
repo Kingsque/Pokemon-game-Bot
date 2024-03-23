51d2c27914e6b5a40962623f1fc452d6fe113cad
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-23 15:44:49 +0530

    Create eredeemcard.js

---
+++ src/Commands/Event/eredeemcard.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: 'eredeemcard',
+  aliases: ['ebuy'],
+  exp: 0,
+  cool: 4,
+  react: "✅",
+  category: 'event',
+  description: 'Claim the card',
+  async execute(client, arg, M) {
+  const mode = await client.event.get(`EVENTS`);
+
+if (mode === 'OFF') {
+    return M.reply('🟥No event is going currently!');
+}
+  
+  M.reply('hi')
+  
+  }
+ }
