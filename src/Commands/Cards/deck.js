ee3c8b5f85e463d8b62ad0e3173fafb884047b6f
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-24 12:28:47 +0530

    Update deck.js

--- src/Commands/Cards/deck.js
+++ src/Commands/Cards/deck.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 const { createCanvas, loadImage } = require('canvas');
 const fs = require('fs');
 const path = require('path');
-const { createDeflate } = require('zlib');
 
 module.exports = {
   name: 'deck',
@@ -16,7 +15,9 @@ module.exports = {
     const deck = await client.DB.get(`${M.sender}_Deck`);
     if (!deck || deck.length === 0) {
       M.reply('No Deck Found');
+      return;
     } 
+
     try {
       const maxCardsInDeck = 12;
       const cardsToMove = deck.slice(maxCardsInDeck);
@@ -24,6 +25,7 @@ module.exports = {
       const collection = await client.DB.get(`${M.sender}_Collection`) || [];
       await client.DB.set(`${M.sender}_Collection`, [...collection, ...cardsToMove]);
       await client.DB.set(`${M.sender}_Deck`, cardsToKeep);
+      
       const bgPath = path.join(__dirname, '../../Helpers/bg.json');
       const bgData = require(bgPath);
       const backgroundTitle = await client.DB.get(`${M.sender}_BG`);
@@ -41,31 +43,35 @@ module.exports = {
         const index = parseInt(arg) - 1; // The index in the array is 0-based
         if (isNaN(index) || index < 0 || index >= deck.length) {
           M.reply(`Invalid card index. Your deck has ${deck.length} cards.`);
+          return;
+        }
+
+        const card = deck[index].split('-');
+        const filePath = path.join(__dirname, '../../Helpers/card.json');
+        const data = require(filePath);
+        const cardsInTier = data.filter((cardData) => cardData.tier === card[1]);
+        const cardData = cardsInTier.find((cardData) => cardData.title === card[0]);
+        const cardUrl = cardData.url;
+
+        let text = `🃏 Total Deck Cards: ${deck.length}\n\n🏮 Username: ${(await client.contact.getContact(M.sender, client)).username}` 
+        text += `\n*#${index + 1}*\n🃏 *Name:* ${card[0]}\n`.concat(`🪄 *Tier:* ${card[1]} \n`)
+        
+        const file = await client.utils.getBuffer(cardUrl);
+        if (cardUrl.endsWith('.gif')) {
+          const giffed = await client.utils.gifToMp4(file);
+          await client.sendMessage(M.from, {
+            video: giffed,
+            gifPlayback: true,
+            caption: text
+          });
         } else {
-          const card = deck[index].split('-');
-          const filePath = path.join(__dirname, '../../Helpers/card.json');
-          const data = require(filePath);
-          const cardsInTier = data.filter((cardData) => cardData.tier === card[1]);
-          const cardData = cardsInTier.find((cardData) => cardData.title === card[0]);
-          const cardUrl = cardData.url;
-          let text = `🃏 Total Deck Cards: ${deck.length}\n\n🏮 Username: ${(await client.contact.getContact(M.sender, client)).username}` 
-          text += `\n*#${index + 1}*\n🃏 *Name:* ${card[0]}\n`.concat(`🪄 *Tier:* ${card[1]} \n`)
-          const file = await client.utils.getBuffer(cardUrl);
-          if (cardUrl.endsWith('.gif')) {
-            const giffed = await client.utils.gifToMp4(file);
-            await client.sendMessage(M.from, {
-              video: giffed,
-              gifPlayback: true,
-              caption: text
-            });
-          } else {
-            await client.sendMessage(M.from, {image: {url: cardUrl}, caption: text}, {quoted: M});
-          }
+          await client.sendMessage(M.from, {image: {url: cardUrl}, caption: text}, {quoted: M});
         }
       } else {
         const images = [];
         let cardText = "";
-        const cardSet = new Set()
+        const cardSet = new Set();
+        
         for (let i = 0; i < deck.length; i++) {
           const card = deck[i].split('-');
           const filePath = path.join(__dirname, '../../Helpers/card.json');
@@ -74,6 +80,7 @@ module.exports = {
           const cardData = cardsInTier.find((cardData) => cardData.title === card[0]);
           const cardKey = `${cardData.title}-${card[1]}`;
           let cardUrl = cardData.url;
+
           if (!cardSet.has(cardKey)) {
             cardSet.add(cardKey);
             images.push(cardUrl);
@@ -87,11 +94,13 @@ module.exports = {
         const ctx = canvas.getContext('2d');
         const backgroundImage = await loadImage(backgroundImageUrl);
         ctx.drawImage(backgroundImage, 0, 0, canvasWidth, canvasHeight);
-        const imageWidth = 350;
-        const imageHeight = 450;
+        
+        const imageWidth = 500;
+        const imageHeight = 650;
         const imagePadding = 10;
         const imagesPerRow = 3;
-        const rows = 4;
+        const rows = Math.ceil(images.length / imagesPerRow);
+        
         const xStart = (canvasWidth - (imageWidth * imagesPerRow + imagePadding * (imagesPerRow - 1))) / 2;
         const yStart = (canvasHeight - (imageHeight * rows + imagePadding * (rows - 1))) / 2;
         
@@ -107,6 +116,7 @@ module.exports = {
         const buffer = canvas.toBuffer('image/png');
         fs.writeFileSync(filePath, buffer);
         const caption = `${(await client.contact.getContact(M.sender, client)).username}'s Deck\n\n Total Cards: ${deck.length}\n${cardText}`;
+        
         client.sendMessage(M.from, {
           image: {url: filePath},
           caption: caption
