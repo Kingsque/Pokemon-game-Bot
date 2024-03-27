be6eaaee6053b32506b7a02447dc0d8d5896c6ad
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-27 15:30:00 +0530

    Create Pokemon.js

---
+++ src/Commands/Pokemon/Pokemon.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+
+module.exports = {
+  name: "pokemon",
+  aliases: ["poke"],
+  exp: 0,
+  cool: 4,
+  react: "📋",
+  category: "pokemon",
+  description: "View Pokémon details",
+  async execute(client, args, message) {
+    try {
+      if (!args.length) {
+        return message.channel.send('Please provide the name or ID of the Pokémon you want to search for.');
+      }
+
+      const pokemonName = args[0].toLowerCase();
+
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      const speciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`);
+
+      const pokemon = response.data;
+      const species = speciesResponse.data;
+
+      const name = pokemon.name;
+      const types = pokemon.types.map(type => type.type.name);
+      const image = pokemon.sprites.front_default;
+      const abilities = pokemon.abilities.map(ability => ability.ability.name);
+      const stats = pokemon.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`);
+      const height = pokemon.height;
+      const weight = pokemon.weight;
+      const flavorText = species.flavor_text_entries.find(entry => entry.language.name === 'en').flavor_text;
+
+      // Fetch evolution chain
+      const evolutionResponse = await axios.get(species.evolution_chain.url);
+      let evolutionChain = evolutionResponse.data.chain;
+      let evolutionDetails = `**Evolution Chain:** ${evolutionChain.species.name}`;
+
+      while (evolutionChain.evolves_to.length > 0) {
+        evolutionChain = evolutionChain.evolves_to[0];
+        evolutionDetails += ` -> ${evolutionChain.species.name}`;
+      }
+
+      const messageContent = `**Name:** ${name}\n**Types:** ${types.join(', ')}\n**Abilities:** ${abilities.join(', ')}\n**Height:** ${height} dm\n**Weight:** ${weight} hg\n**Stats:**\n${stats.join('\n')}\n**Flavor Text:**\n${flavorText}\n${evolutionDetails}`;
+
+      await client.sendMessage(message.jid, {
+        image: {
+          url: image,
+        },
+        caption: messageContent,
+      });
+    } catch (error) {
+      console.error('Error fetching Pokémon data:', error);
+      message.channel.send('Error fetching Pokémon data. Please try again later.');
+    }
+  },
+};
