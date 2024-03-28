2686057bead9869954f67bc022f183079e357e5f
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-28 11:47:02 +0530

    Update deck.js

--- src/Commands/Cards/deck.js
+++ src/Commands/Cards/deck.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { createCanvas, loadImage } = require('canvas');
 const fs = require('fs');
 const path = require('path');
-const zlib = require('zlib');
+const { createDeflate } = require('zlib');
 
 module.exports = {
   name: 'deck',
@@ -38,40 +38,35 @@ module.exports = {
         }
       }
 
-      const gifCards = [];
-      const images = [];
-      let cardText = "";
-      const cardSet = new Set();
-      
-      if (arg) {
-        const index = parseInt(arg) - 1;
+      if (arg) { 
+        const index = parseInt(arg) - 1; // The index in the array is 0-based
         if (isNaN(index) || index < 0 || index >= deck.length) {
           M.reply(`Invalid card index. Your deck has ${deck.length} cards.`);
-          return;
-        }
-        const card = deck[index].split('-');
-        const filePath = path.join(__dirname, '../../Helpers/card.json');
-        const data = require(filePath);
-        const cardsInTier = data.filter((cardData) => cardData.tier === card[1]);
-        const cardData = cardsInTier.find((cardData) => cardData.title === card[0]);
-        const cardUrl = cardData.url;
-        cardText = `🃏 Total Deck Cards: ${deck.length}\n\n🏮 Username: ${(await client.contact.getContact(M.sender, client)).username}\n*#${index + 1}*\n🃏 *Name:* ${card[0]}\n🪄 *Tier:* ${card[1]} \n`;
-        if (cardUrl.endsWith('.gif')) {
-          gifCards.push(cardUrl);
         } else {
-          images.push(cardUrl);
-        }
-
-        // Send single card
-        if (cardUrl.endsWith('.gif')) {
-          const imageBuffer = await client.utils.webpToMp4(await client.utils.getBuffer(cardUrl));
-          await client.sendMessage(M.from, { video: imageBuffer, gifPlayback: true, caption: cardText });
-        } else {
-          await client.sendMessage(M.from, { image: { url: cardUrl }, caption: cardText });
+          const card = deck[index].split('-');
+          const filePath = path.join(__dirname, '../../Helpers/card.json');
+          const data = require(filePath);
+          const cardsInTier = data.filter((cardData) => cardData.tier === card[1]);
+          const cardData = cardsInTier.find((cardData) => cardData.title === card[0]);
+          const cardUrl = cardData.url;
+          let text = `🃏 Total Deck Cards: ${deck.length}\n\n🏮 Username: ${(await client.contact.getContact(M.sender, client)).username} \n*#${index + 1}*\n🃏 *Name:* ${card[0]}\n🪄 *Tier:* ${card[1]} \n`;
+          const file = await client.utils.getBuffer(cardUrl);
+          if (cardUrl.endsWith('.gif')) {
+            const giffed = await client.utils.gifToMp4(file);
+            await client.sendMessage(M.from, {
+              video: giffed,
+              gifPlayback: true,
+              caption: text
+            });
+          } else {
+            await client.sendMessage(M.from, {image: {url: cardUrl}, caption: text}, {quoted: M});
+          }
         }
       } else {
+        const images = [];
+        let cardText = "";
+        const cardSet = new Set();
         for (let i = 0; i < deck.length; i++) {
-          let text = "";
           const card = deck[i].split('-');
           const filePath = path.join(__dirname, '../../Helpers/card.json');
           const data = require(filePath);
@@ -79,29 +74,13 @@ module.exports = {
           const cardData = cardsInTier.find((cardData) => cardData.title === card[0]);
           const cardKey = `${cardData.title}-${card[1]}`;
           let cardUrl = cardData.url;
-
-          if (cardUrl.endsWith('.gif')) {
-            gifCards.push(cardUrl);
-          } else {
-            images.push(cardUrl);
-          }
-
           if (!cardSet.has(cardKey)) {
             cardSet.add(cardKey);
-            text += `🔰Card ${i+1}:\n🌟Tier: ${card[1]}\n💎Name ${card[0]}\n\n`;
+            images.push(cardUrl);
           }
+          cardText += `🔰Card ${i+1}:\n🌟Tier: ${card[1]}\n💎Name ${card[0]}\n\n`;
         }
-
-        // Convert GIF cards to images
-        for (const gifCardUrl of gifCards) {
-          const imageBuffer = await client.utils.webpToMp4(await client.utils.getBuffer(gifCardUrl));
-          const directory = require('os').tmpdir();
-          const filePath = path.join(directory, `gif_card_${gifCards.indexOf(gifCardUrl)}.png`);
-          fs.writeFileSync(filePath, imageBuffer);
-          images.push(filePath);
-        }
-
-        // Send collage
+        
         const canvasWidth = 1050;
         const canvasHeight = 1800;
         const canvas = createCanvas(canvasWidth, canvasHeight);
@@ -115,25 +94,28 @@ module.exports = {
         const rows = 4;
         const xStart = (canvasWidth - (imageWidth * imagesPerRow + imagePadding * (imagesPerRow - 1))) / 2;
         const yStart = (canvasHeight - (imageHeight * rows + imagePadding * (rows - 1))) / 2;
-
+        
         for (let i = 0; i < images.length; i++) {
           const image = await loadImage(images[i]);
           const x = xStart + (i % imagesPerRow) * (imageWidth + imagePadding);
           const y = yStart + Math.floor(i / imagesPerRow) * (imageHeight + imagePadding);
           ctx.drawImage(image, x, y, imageWidth, imageHeight);
         }
-
+        
         const directory = require('os').tmpdir();
         const filePath = path.join(directory, 'collage.png');
         const buffer = canvas.toBuffer('image/png');
         fs.writeFileSync(filePath, buffer);
-        const caption = `${(await client.contact.getContact(M.sender, client)).username}'s Deck\n\n Total Cards: ${deck.length}\n${text}`;
-        await client.sendMessage(M.from, { image: { url: filePath }, caption: caption });
-      }
+        const caption = `${(await client.contact.getContact(M.sender, client)).username} 's Deck\n\n Total Cards: ${deck.length}\n${cardText}`;
+        client.sendMessage(M.from, {
+          image: {url: filePath},
+          caption: caption
+        });
+      } 
     } catch(err) {
       console.log(err);
-      await client.sendMessage(M.from, { image: { url: `${client.utils.errorChan()}` }, caption: `${client.utils.greetings()} Error-Chan Dis\n\nError:\n${err}` });
+      await client.sendMessage(M.from, {image: {url: `${client.utils.errorChan()}`}, caption: `${client.utils.greetings()} Error-Chan Dis\n\nError:\n${err}`});
     }
   },
 };
-             
+            
