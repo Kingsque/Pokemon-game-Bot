3a7e18815d14c832ac84453ad53b957a92f7855f
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-28 14:12:06 +0530

    Update manga.js

--- src/Commands/Weeb/manga.js
+++ src/Commands/Weeb/manga.js
@@ -1,4 +1,4 @@
-const { Manga } = require('@shineiichijo/marika')
+const { Manga } = require('@shineiichijo/marika');
 
 module.exports = {
     name: 'anime',
@@ -9,36 +9,36 @@ module.exports = {
     react: "✅",
     description: 'Gives you the info of the anime',
     async execute(client, arg, M) {
-        if (!arg) return void M.reply('Provide a query for the search, Baka!')
-        const query = arg.trim()
-        await new Manga()
-            .searchManga(query)
-            .then(async ({ data }) => {
-                const result = data[0]
-                let text = ''
-                text += `🎀 *Title:* ${result.title}\n`
-                text += `🎋 *Format:* ${result.type}\n`
-                text += `📈 *Status:* ${this.helper.utils.capitalize(result.status.replace(/\_/g, ' '))}\n`
-                text += `🍥 *Total chapters:* ${result.chapters}\n`
-                text += `🎈 *Total volumes:* ${result.volumes}\n`
-                text += `🧧 *Genres:* ${result.genres.map((genre) => genre.name).join(', ')}\n`
-                text += `💫 *Published on:* ${result.published.from}\n`
-                text += `🎗 *Ended on:* ${result.published.to}\n`
-                text += `🎐 *Popularity:* ${result.popularity}\n`
-                text += `🎏 *Favorites:* ${result.favorites}\n`
-                text += `🏅 *Rank:* ${result.rank}\n\n`
-                if (result.background !== null) text += `🎆 *Background:* ${result.background}*\n\n`
-                text += `❄ *Description:* ${result.synopsis}`
-                const image = result.images.jpg.large_image_url
+        if (!arg) return void M.reply('Provide a query for the search, Baka!');
+        const query = arg.trim();
+        try {
+            const { data } = await new Manga().searchManga(query);
+            const result = data[0];
+            let text = '';
+            text += `🎀 *Title:* ${result.title}\n`;
+            text += `🎋 *Format:* ${result.type}\n`;
+            text += `📈 *Status:* ${client.utils.capitalize(result.status.replace(/\_/g, ' '))}\n`;
+            text += `🍥 *Total chapters:* ${result.chapters}\n`;
+            text += `🎈 *Total volumes:* ${result.volumes}\n`;
+            text += `🧧 *Genres:* ${result.genres.map((genre) => genre.name).join(', ')}\n`;
+            text += `💫 *Published on:* ${result.published.from}\n`;
+            text += `🎗 *Ended on:* ${result.published.to}\n`;
+            text += `🎐 *Popularity:* ${result.popularity}\n`;
+            text += `🎏 *Favorites:* ${result.favorites}\n`;
+            text += `🏅 *Rank:* ${result.rank}\n\n`;
+            if (result.background !== null) text += `🎆 *Background:* ${result.background}*\n\n`;
+            text += `❄ *Description:* ${result.synopsis}`;
+            const image = result.images.jpg.large_image_url;
 
-                  await client.sendMessage(M.from, {
+            await client.sendMessage(M.from, {
                 image: {
                     url: image,
                 },
                 caption: text,
             });
-            } catch (err) {
+        } catch (err) {
             console.error('Error fetching anime information:', err);
             M.reply('An error occurred while fetching anime information.');
         }
     }
+};
