32d67fe73bd798fe3d616cd1b7583ffbb00ba263
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-28 21:59:06 +0530

    Update aniNews.js

--- src/Commands/Weeb/aniNews.js
+++ src/Commands/Weeb/aniNews.js
@@ -7,20 +7,22 @@ module.exports = {
     exp: 15,
     cool: 4,
     react: "✅",
-    description: 'Provides news about anime',
+    usage: 'Use :aninews',
+    description: 'Provides news about anime world by aurora',
     async execute(client, arg, M) {
         try {
-            const news = await new NEWS.ANN().fetchNewsFeeds(); // Updated fetch method
-            for (let i = 0; i < 5 && i < news.length; i++) { // Added condition to avoid errors if less than 5 news articles are available
+            const news = await new NEWS.ANN().fetchNewsFeeds();
+            for (let i = 0; i < 5 && i < news.length; i++) {
                 const article = news[i];
-                const topics = article.topics.join('\n'); // Improved formatting for topics
-                const previewIntro = article.preview.intro.replace(/\n/g, '\n\n'); // Improved formatting for intro
-                const previewFull = article.preview.full.replace(/\n/g, '\n\n'); // Improved formatting for full description
+                const topics = article.topics.join('\n');
+                const previewIntro = article.preview.intro.replace(/\n/g, '\n\n');
+                const previewFull = article.preview.full.replace(/\n/g, '\n\n');
+                const footerText = "© _Team Aurora𝄞"; // Add your footer text here
                 await client.sendMessage(M.from, {
                     image: {
                         url: article.thumbnail
                     },
-                    caption: `===== ANIME NEWS =====\n*Title*: ${article.title}\n*ID*: ${article.id}\n*Topics*: ${topics}\n*Uploaded At*: ${article.uploadedAt}\n*Preview*:-\n\n*Intro*: ${previewIntro}\n\n*Description*: ${previewFull}\n*Link*: ${article.url}`
+                    caption: `*❯─『 ANIME NEWS 』─❮*\n*Title*: ${article.title}\n*ID*: ${article.id}\n*Topics*: ${topics}\n*Uploaded At*: ${article.uploadedAt}\n*Preview*:-\n\n*Intro*: ${previewIntro}\n\n*Description*: ${previewFull}\n*Link*: ${article.url}\n\n${footerText}`
                 });
             }
         } catch (err) {
