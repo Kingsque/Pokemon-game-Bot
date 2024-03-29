2f406bf0606f7aff207a52524aadc8c5fa672413
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-29 15:57:17 +0530

    Update pokeStats.js

--- src/Helpers/pokeStats.js
+++ src/Helpers/pokeStats.js
@@ -1,306 +1,307 @@
 const pokeLevel = {
   level1: {
-    exp: 100
+    pokeExp: 100
   },
   level2: {
-    exp: 200
+    pokeExp: 200
   },
   level3: {
-    exp: 300
+    pokeExp: 300
   },
   level4: {
-    exp: 400
+    pokeExp: 400
   },
   level5: {
-    exp: 500
+    pokeExp: 500
   },
   level6: {
-    exp: 600
+    pokeExp: 600
   },
   level7: {
-    exp: 700
+    pokeExp: 700
   },
   level8: {
-    exp: 800
+    pokeExp: 800
   },
   level9: {
-    exp: 900
+    pokeExp: 900
   },
   level10: {
-    exp: 1000
+    pokeExp: 1000
   },
   level11: {
-    exp: 1100
+    pokeExp: 1100
   },
   level12: {
-    exp: 1200
+    pokeExp: 1200
   },
   level13: {
-    exp: 1300
+    pokeExp: 1300
   },
   level14: {
-    exp: 1400
+    pokeExp: 1400
   },
   level15: {
-    exp: 1500
+    pokeExp: 1500
   },
   level16: {
-    exp: 1600
+    pokeExp: 1600
   },
   level17: {
-    exp: 1700
+    pokeExp: 1700
   },
   level18: {
-    exp: 1800
+    pokeExp: 1800
   },
   level19: {
-    exp: 1900
+    pokeExp: 1900
   },
   level20: {
-    exp: 2000
+    pokeExp: 2000
   },
   level21: {
-    exp: 2100
+    pokeExp: 2100
   },
   level22: {
-    exp: 2200
+    pokeExp: 2200
   },
   level23: {
-    exp: 2300
+    pokeExp: 2300
   },
   level24: {
-    exp: 2400
+    pokeExp: 2400
   },
   level25: {
-    exp: 2500
+    pokeExp: 2500
   },
   level26: {
-    exp: 2600
+    pokeExp: 2600
   },
   level27: {
-    exp: 2700
+    pokeExp: 2700
   },
   level28: {
-    exp: 2800
+    pokeExp: 2800
   },
   level29: {
-    exp: 2900
+    pokeExp: 2900
   },
   level30: {
-    exp: 3000
+    pokeExp: 3000
   },
   level31: {
-    exp: 3100
+    pokeExp: 3100
   },
   level32: {
-    exp: 3200
+    pokeExp: 3200
   },
   level33: {
-    exp: 3300
+    pokeExp: 3300
   },
   level34: {
-    exp: 3400
+    pokeExp: 3400
   },
   level35: {
-    exp: 3500
+    pokeExp: 3500
   },
   level36: {
-    exp: 3600
+    pokeExp: 3600
   },
   level37: {
-    exp: 3700
+    pokeExp: 3700
   },
   level38: {
-    exp: 3800
+    pokeExp: 3800
   },
   level39: {
-    exp: 3900
+    pokeExp: 3900
   },
   level40: {
-    exp: 4000
+    pokeExp: 4000
   },
   level41: {
-    exp: 4100
+    pokeExp: 4100
   },
   level42: {
-    exp: 4200
+    pokeExp: 4200
   },
   level43: {
-    exp: 4300
+    pokeExp: 4300
   },
   level44: {
-    exp: 4400
+    pokeExp: 4400
   },
   level45: {
-    exp: 4500
+    pokeExp: 4500
   },
   level46: {
-    exp: 4600
+    pokeExp: 4600
   },
   level47: {
-    exp: 4700
+    pokeExp: 4700
   },
   level48: {
-    exp: 4800
+    pokeExp: 4800
   },
   level49: {
-    exp: 4900
+    pokeExp: 4900
   },
   level50: {
-    exp: 5000
+    pokeExp: 5000
   },
   level51: {
-    exp: 5100
+    pokeExp: 5100
   },
   level52: {
-    exp: 5200
+    pokeExp: 5200
   },
   level53: {
-    exp: 5300
+    pokeExp: 5300
   },
   level54: {
-    exp: 5400
+    pokeExp: 5400
   },
   level55: {
-    exp: 5500
+    pokeExp: 5500
   },
   level56: {
-    exp: 5600
+    pokeExp: 5600
   },
   level57: {
-    exp: 5700
+    pokeExp: 5700
   },
   level58: {
-    exp: 5800
+    pokeExp: 5800
   },
   level59: {
-    exp: 5900
+    pokeExp: 5900
   },
   level60: {
-    exp: 6000
+    pokeExp: 6000
   },
   level61: {
-    exp: 6100
+    pokeExp: 6100
   },
   level62: {
-    exp: 6200
+    pokeExp: 6200
   },
   level63: {
-    exp: 6300
+    pokeExp: 6300
   },
   level64: {
-    exp: 6400
+    pokeExp: 6400
   },
   level65: {
-    exp: 6500
+    pokeExp: 6500
   },
   level66: {
-    exp: 6600
+    pokeExp: 6600
   },
   level67: {
-    exp: 6700
+    pokeExp: 6700
   },
   level68: {
-    exp: 6800
+    pokeExp: 6800
   },
   level69: {
-    exp: 6900
+    pokeExp: 6900
   },
   level70: {
-    exp: 7000
+    pokeExp: 7000
   },
   level71: {
-    exp: 7100
+    pokeExp: 7100
   },
   level72: {
-    exp: 7200
+    pokeExp: 7200
   },
   level73: {
-    exp: 7300
+    pokeExp: 7300
   },
   level74: {
-    exp: 7400
+    pokeExp: 7400
   },
   level75: {
-    exp: 7500
+    pokeExp: 7500
   },
   level76: {
-    exp: 7600
+    pokeExp: 7600
   },
   level77: {
-    exp: 7700
+    pokeExp: 7700
   },
   level78: {
-    exp: 7800
+    pokeExp: 7800
   },
   level79: {
-    exp: 7900
+    pokeExp: 7900
   },
   level80: {
-    exp: 8000
+    pokeExp: 8000
   },
   level81: {
-    exp: 8100
+    pokeExp: 8100
   },
   level82: {
-    exp: 8200
+    pokeExp: 8200
   },
   level83: {
-    exp: 8300
+    pokeExp: 8300
   },
   level84: {
-    exp: 8400
+    pokeExp: 8400
   },
   level85: {
-    exp: 8500
+    pokeExp: 8500
   },
   level86: {
-    exp: 8600
+    pokeExp: 8600
   },
   level87: {
-    exp: 8700
+    pokeExp: 8700
   },
   level88: {
-    exp: 8800
+    pokeExp: 8800
   },
   level89: {
-    exp: 8900
+    pokeExp: 8900
   },
   level90: {
-    exp: 9000
+    pokeExp: 9000
   },
   level91: {
-    exp: 9100
+    pokeExp: 9100
   },
   level92: {
-    exp: 9200
+    pokeExp: 9200
   },
   level93: {
-    exp: 9300
+    pokeExp: 9300
   },
   level94: {
-    exp: 9400
+    pokeExp: 9400
   },
   level95: {
-    exp: 9500
+    pokeExp: 9500
   },
   level96: {
-    exp: 9600
+    pokeExp: 9600
   },
   level97: {
-    exp: 9700
+    pokeExp: 9700
   },
   level98: {
-    exp: 9800
+    pokeExp: 9800
   },
   level99: {
-    exp: 9900
+    pokeExp: 9900
   },
   level100: {
-    exp: 10000
+    pokeExp: 10000
   }
 };
 
 module.exports = {
   pokeLevel
 };
+    
