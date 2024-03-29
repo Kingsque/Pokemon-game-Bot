d0919667eee8406cd0e7a697a795e6d53cc66307
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-29 15:38:27 +0530

    Update pokeStats.js

--- src/Helpers/pokeStats.js
+++ src/Helpers/pokeStats.js
@@ -1,107 +1,306 @@
 const pokeLevel = {
-  level1: 100,
-  level2: 200,
-  level3: 300,
-  level4: 400,
-  level5: 500,
-  level6: 600,
-  level7: 700,
-  level8: 800,
-  level9: 900,
-  level10: 1000,
-  level11: 1100,
-  level12: 1200,
-  level13: 1300,
-  level14: 1400,
-  level15: 1500,
-  level16: 1600,
-  level17: 1700,
-  level18: 1800,
-  level19: 1900,
-  level20: 2000,
-  level21: 2100,
-  level22: 2200,
-  level23: 2300,
-  level24: 2400,
-  level25: 2500,
-  level26: 2600,
-  level27: 2700,
-  level28: 2800,
-  level29: 2900,
-  level30: 3000,
-  level31: 3100,
-  level32: 3200,
-  level33: 3300,
-  level34: 3400,
-  level35: 3500,
-  level36: 3600,
-  level37: 3700,
-  level38: 3800,
-  level39: 3900,
-  level40: 4000,
-  level41: 4100,
-  level42: 4200,
-  level43: 4300,
-  level44: 4400,
-  level45: 4500,
-  level46: 4600,
-  level47: 4700,
-  level48: 4800,
-  level49: 4900,
-  level50: 5000,
-  level51: 5100,
-  level52: 5200,
-  level53: 5300,
-  level54: 5400,
-  level55: 5500,
-  level56: 5600,
-  level57: 5700,
-  level58: 5800,
-  level59: 5900,
-  level60: 6000,
-  level61: 6100,
-  level62: 6200,
-  level63: 6300,
-  level64: 6400,
-  level65: 6500,
-  level66: 6600,
-  level67: 6700,
-  level68: 6800,
-  level69: 6900,
-  level70: 7000,
-  level71: 7100,
-  level72: 7200,
-  level73: 7300,
-  level74: 7400,
-  level75: 7500,
-  level76: 7600,
-  level77: 7700,
-  level78: 7800,
-  level79: 7900,
-  level80: 8000,
-  level81: 8100,
-  level82: 8200,
-  level83: 8300,
-  level84: 8400,
-  level85: 8500,
-  level86: 8600,
-  level87: 8700,
-  level88: 8800,
-  level89: 8900,
-  level90: 9000,
-  level91: 9100,
-  level92: 9200,
-  level93: 9300,
-  level94: 9400,
-  level95: 9500,
-  level96: 9600,
-  level97: 9700,
-  level98: 9800,
-  level99: 9900,
-  level100: 10000
+  level1: {
+    exp: 100
+  },
+  level2: {
+    exp: 200
+  },
+  level3: {
+    exp: 300
+  },
+  level4: {
+    exp: 400
+  },
+  level5: {
+    exp: 500
+  },
+  level6: {
+    exp: 600
+  },
+  level7: {
+    exp: 700
+  },
+  level8: {
+    exp: 800
+  },
+  level9: {
+    exp: 900
+  },
+  level10: {
+    exp: 1000
+  },
+  level11: {
+    exp: 1100
+  },
+  level12: {
+    exp: 1200
+  },
+  level13: {
+    exp: 1300
+  },
+  level14: {
+    exp: 1400
+  },
+  level15: {
+    exp: 1500
+  },
+  level16: {
+    exp: 1600
+  },
+  level17: {
+    exp: 1700
+  },
+  level18: {
+    exp: 1800
+  },
+  level19: {
+    exp: 1900
+  },
+  level20: {
+    exp: 2000
+  },
+  level21: {
+    exp: 2100
+  },
+  level22: {
+    exp: 2200
+  },
+  level23: {
+    exp: 2300
+  },
+  level24: {
+    exp: 2400
+  },
+  level25: {
+    exp: 2500
+  },
+  level26: {
+    exp: 2600
+  },
+  level27: {
+    exp: 2700
+  },
+  level28: {
+    exp: 2800
+  },
+  level29: {
+    exp: 2900
+  },
+  level30: {
+    exp: 3000
+  },
+  level31: {
+    exp: 3100
+  },
+  level32: {
+    exp: 3200
+  },
+  level33: {
+    exp: 3300
+  },
+  level34: {
+    exp: 3400
+  },
+  level35: {
+    exp: 3500
+  },
+  level36: {
+    exp: 3600
+  },
+  level37: {
+    exp: 3700
+  },
+  level38: {
+    exp: 3800
+  },
+  level39: {
+    exp: 3900
+  },
+  level40: {
+    exp: 4000
+  },
+  level41: {
+    exp: 4100
+  },
+  level42: {
+    exp: 4200
+  },
+  level43: {
+    exp: 4300
+  },
+  level44: {
+    exp: 4400
+  },
+  level45: {
+    exp: 4500
+  },
+  level46: {
+    exp: 4600
+  },
+  level47: {
+    exp: 4700
+  },
+  level48: {
+    exp: 4800
+  },
+  level49: {
+    exp: 4900
+  },
+  level50: {
+    exp: 5000
+  },
+  level51: {
+    exp: 5100
+  },
+  level52: {
+    exp: 5200
+  },
+  level53: {
+    exp: 5300
+  },
+  level54: {
+    exp: 5400
+  },
+  level55: {
+    exp: 5500
+  },
+  level56: {
+    exp: 5600
+  },
+  level57: {
+    exp: 5700
+  },
+  level58: {
+    exp: 5800
+  },
+  level59: {
+    exp: 5900
+  },
+  level60: {
+    exp: 6000
+  },
+  level61: {
+    exp: 6100
+  },
+  level62: {
+    exp: 6200
+  },
+  level63: {
+    exp: 6300
+  },
+  level64: {
+    exp: 6400
+  },
+  level65: {
+    exp: 6500
+  },
+  level66: {
+    exp: 6600
+  },
+  level67: {
+    exp: 6700
+  },
+  level68: {
+    exp: 6800
+  },
+  level69: {
+    exp: 6900
+  },
+  level70: {
+    exp: 7000
+  },
+  level71: {
+    exp: 7100
+  },
+  level72: {
+    exp: 7200
+  },
+  level73: {
+    exp: 7300
+  },
+  level74: {
+    exp: 7400
+  },
+  level75: {
+    exp: 7500
+  },
+  level76: {
+    exp: 7600
+  },
+  level77: {
+    exp: 7700
+  },
+  level78: {
+    exp: 7800
+  },
+  level79: {
+    exp: 7900
+  },
+  level80: {
+    exp: 8000
+  },
+  level81: {
+    exp: 8100
+  },
+  level82: {
+    exp: 8200
+  },
+  level83: {
+    exp: 8300
+  },
+  level84: {
+    exp: 8400
+  },
+  level85: {
+    exp: 8500
+  },
+  level86: {
+    exp: 8600
+  },
+  level87: {
+    exp: 8700
+  },
+  level88: {
+    exp: 8800
+  },
+  level89: {
+    exp: 8900
+  },
+  level90: {
+    exp: 9000
+  },
+  level91: {
+    exp: 9100
+  },
+  level92: {
+    exp: 9200
+  },
+  level93: {
+    exp: 9300
+  },
+  level94: {
+    exp: 9400
+  },
+  level95: {
+    exp: 9500
+  },
+  level96: {
+    exp: 9600
+  },
+  level97: {
+    exp: 9700
+  },
+  level98: {
+    exp: 9800
+  },
+  level99: {
+    exp: 9900
+  },
+  level100: {
+    exp: 10000
+  }
 };
 
 module.exports = {
   pokeLevel
-}
-  
+};
