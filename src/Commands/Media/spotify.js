530bd61583a0530bc2947ba3c23ec6aa73d6d087
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-30 12:37:07 +0530

    Update spotify.js

--- src/Commands/Media/spotify.js
+++ src/Commands/Media/spotify.js
@@ -10,22 +10,33 @@ module.exports = {
     description: 'Downloads given Spotify track and sends it as Audio',
     async execute(client, arg, M) {
         try {
-            let context = arg.includes('https://open.spotify.com/')
-            if (!context) return M.reply('Where is the url of spotify?')
-            const spotify = new Spotify(context)
-            const info = await spotify.getInfo() // Changed this line
-            if (info.error) return M.reply('Provide a valid spotify track URL, Baka!')
-            const { name, artists, album_name, release_date, cover_url } = info
-            const details = `🎧 *Title:* ${name || ''}\n🎤 *Artists:* ${(artists || []).join(
-                ','
-            )}\n💽 *Album:* ${album_name}\n📆 *Release Date:* ${release_date || ''}`
+            if (!arg || !arg.includes('https://open.spotify.com/')) {
+                return M.reply('Please provide a valid Spotify track URL.');
+            }
+            
+            const spotify = new Spotify(arg);
+            const info = await spotify.getInfo();
+            
+            if (info.error) {
+                return M.reply('Provide a valid Spotify track URL.');
+            }
+            
+            const { name, artists, album_name, release_date, cover_url } = info;
+            const details = `🎧 *Title:* ${name || ''}\n🎤 *Artists:* ${(artists || []).join(',')}\n💽 *Album:* ${album_name}\n📆 *Release Date:* ${release_date || ''}`;
+            
             const response = await client.sendMessage(
                 M.from,
                 { image: { url: cover_url }, caption: details },
                 { quoted: M.message }
-            )
-            const buffer = await spotify.download()
-            await client.sendMessage(M.from, { audio: buffer }, { quoted: response })
+            );
+            
+            const buffer = await spotify.download();
+            
+            if (buffer) {
+                await client.sendMessage(M.from, { audio: buffer }, { quoted: response });
+            } else {
+                M.reply('Failed to download the Spotify track.');
+            }
         } catch (error) {
             console.error(error);
             M.reply('An error occurred while downloading the Spotify track.');
