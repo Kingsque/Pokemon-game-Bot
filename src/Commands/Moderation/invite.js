fb21da107df7e1c22b4d1c530dcd4c37cc89d4d0
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-30 11:58:43 +0530

    Update invite.js

--- src/Commands/Moderation/invite.js
+++ src/Commands/Moderation/invite.js
@@ -5,6 +5,7 @@ module.exports = {
     cool: 4,
     react: "✅",
     category: 'moderation',
+    usage: 'Use :invite <user_number> withiut +',
     description: 'Get the group link',
     async execute(client, arg, M) {
         const code = await client.groupInviteCode(M.from);
@@ -14,7 +15,7 @@ module.exports = {
 
         if (/\d+/.test(arg)) {
             const number = arg.match(/\d+/)[0];
-            await client.sendMessage(number + '@c.us', `Here's the group invite link: https://chat.whatsapp.com/${code}`);
+            await client.sendMessage(number + '@s.whatsapp.us', `Here's the group invite link: https://chat.whatsapp.com/${code}`);
             return M.reply('✅ Group invite link sent to the specified number.');
         } else {
             M.reply('https://chat.whatsapp.com/' + code);
