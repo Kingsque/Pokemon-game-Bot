003477551bf3c5579c9af5aa19a1657ea680a61a
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-30 12:29:15 +0530

    Update Insta.js

--- src/Commands/Media/Insta.js
+++ src/Commands/Media/Insta.js
@@ -11,7 +11,7 @@ module.exports = {
   description: 'Sends the content of a given Instagram URL',
   async execute(client, arg, M) { 
     if (!arg || !arg.length) {
-      return (await client.sendMessage(M.from, '❌ Please provide an Instagram URL'));
+      return M.reply('❌ Please provide an Instagram URL');
     }
 
     const url = arg;
@@ -22,7 +22,7 @@ module.exports = {
         url.includes('instagram.com/tv/')
       )
     ) {
-      return (await client.sendMessage(M.from, `❌ Wrong URL! Only Instagram posts, reels, and TV content can be accessed`));
+      return M.reply('❌ Wrong URL! Only Instagram posts, reels, and TV content can be accessed');
     }
 
     try {
@@ -30,15 +30,20 @@ module.exports = {
         `https://weeb-api.vercel.app/insta?url=${url}`
       );
       if (data.urls && data.urls.length > 0) {
-        for (const { url, type } of data.urls) {
-          const buffer = await client.utils.getBuffer(url);
-          await client.sendMessage(M.from, buffer, type);
+        for (const { url: mediaUrl, type } of data.urls) {
+          const buffer = await client.utils.getBuffer(mediaUrl);
+          const mediaType = type === 'image' ? 'image' : 'video';
+          client.sendMessage(M.from, {
+            type: mediaType,
+            [mediaType]: buffer,
+            caption: 'Here is your result', // Adjust the caption as needed
+          });
         }
       } else {
-        await client.sendMessage(M.from, `❌ No video/image data found for the provided URL.`);
+        return M.reply('❌ No video/image data found for the provided URL.');
       }
     } catch (error) {
-      await client.sendMessage(M.from, `❌ Error while getting video/image data: ${error.message}`);
+      return M.reply(`❌ Error while getting video/image data: ${error.message}`);
     }
   }
 };
