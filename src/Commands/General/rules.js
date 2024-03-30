c6517fb9b5c9052d645db7a97eb61446140089c9
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-30 21:04:47 +0530

    Update rules.js

--- src/Commands/General/rules.js
+++ src/Commands/General/rules.js
@@ -5,7 +5,8 @@ category: 'general',
 exp: 15,
 cool: 4,
 react: "✅",
-description: 'This is noteboard, guide and rules board. You can check here about updates, about guide or about rules.',
+usage: 'Use :rule',
+description: 'Here you can get the rules of our bot ehivh is made to be maintained and breaking rules have punishments.',
 async execute(client, arg, M) {
 
   const image = await client.utils.getBuffer('https://i.ibb.co/1sbf4Zn/Picsart-24-02-20-16-40-03-063.jpg');
@@ -17,7 +18,8 @@ async execute(client, arg, M) {
   text += `3) *Don't fight or use slang in our official groups*\n`
   text += `4) *Any rule breaking may result in a ban*\n`
   text += `5) *In one auction one user can only win a single card*\n`
-  text += `6) *From card shop a user can only buy a single card from the shop*\n\n`;
+  text += `6) *Dont use by for searching any insulting or nudify or controversial matter*\n`
+  text += `7) *Dont call the bot or send spam messages in the bits DM*\n`
   await client.sendMessage(M.from, { image: { url: image }, caption: text }, { quoted: M });
        }
       } 
