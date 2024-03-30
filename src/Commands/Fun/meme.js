36844768e025971e477d70cb6676c75c76ca6ebd
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-30 21:45:45 +0530

    Update meme.js

--- src/Commands/Fun/meme.js
+++ src/Commands/Fun/meme.js
@@ -7,16 +7,17 @@ module.exports = {
     exp: 16,
     cool: 4,
     react: "✅",
-    description: 'Sends an image of random meme',
+    usage: 'Use :meme',
+    description: 'Sends an image of a random meme',
     async execute(client, arg, M) {
         try {
             const res = await axios.get('https://meme-api.com/gimme');
             if (res.data.success === true) {
                 client.sendMessage(M.from, {
                     image: {
-                        url: res.data.url
-                    },
-                    caption: `${res.data.title}`
+                        url: res.data.url,
+                        caption: res.data.title
+                    }
                 });
             } else {
                 M.reply('Failed to fetch meme.');
