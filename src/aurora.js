0e278ba34d54fe314e069f1bb9412dda54e85222
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-30 14:54:54 +0530

    Update aurora.js

--- src/aurora.js
+++ src/aurora.js
@@ -52,7 +52,7 @@ const start = async () => {
     //Config
     client.name = process.env.NAME || 'Aurora-Private'
     client.prefix = process.env.PREFIX || ':'
-    client.mods = ('917903576495,918961331275,917980329866').split(',')
+    client.mods = ('917903576495,916239664935,917980329866').split(',')
 
     //grouos
     client.groups = groups()
