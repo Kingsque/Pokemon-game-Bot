eaef9ca75c51616ce8bf8d29afa8e02c9c5f610c
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-30 22:01:10 +0530

    Update truth_dare.js

--- src/Commands/Fun/truth_dare.js
+++ src/Commands/Fun/truth_dare.js
@@ -7,6 +7,7 @@ module.exports = {
     exp: 9,
     cool: 4,
     react: "✅",
+    usage: 'Use :truth or :dare',
     description: 'Gives you truth or dare.',
     async execute(client, arg, message) {
         if (!arg) return message.reply('Please specify "truth" or "dare"!');
