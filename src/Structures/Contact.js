2b1dab8d3e1154144a9529f964f9e72dec6f901d
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-30 15:20:48 +0530

    Update Contact.js

--- src/Structures/Contact.js
+++ src/Structures/Contact.js
@@ -1,24 +1,26 @@
-const getContact = async (jid, client) => {
-    // Get contact information from the contact database using the JID
+getContact = async (jid, client) => {
+    // Gets the arr of contacts
     const contact = await client.contactDB.get(jid)
-
-    // If the contact exists, return their name, otherwise return "User"
-    const username = contact ?? 'User'
-
-    return { username, jid }
+    //console.log(contact)
+    if (!contact)
+        return {
+            username: 'User',
+            jid
+        }
+    return {
+        username: contact || 'User',
+        jid
+    }
 }
 
-// Saves an array of contacts to the contact database
-const saveContacts = async (contacts, client) => {
-    // Use Promise.all to concurrently process each contact in the array
-    await Promise.all(
-        contacts.map(async (contact) => {
-            // If the contact has an ID, set their notify value in the database
-            if (contact.id) {
-                await client.contactDB.set(contact.id, contact.notify ?? '')
-            }
-        })
-    )
+saveContacts = async (contacts, client) => {
+    // Saves the contacts
+    for (const contact of contacts) {
+        if (contact.id) {
+            await client.contactDB.set(contact.id, contact.notify || '')
+            //console.log(contact)
+        }
+    }
 }
 
 module.exports = {
