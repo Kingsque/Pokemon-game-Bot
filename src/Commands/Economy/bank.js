4b2043b347ea22a4eaac17d47e944d23bacaaf2d
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-03-31 11:02:02 +0530

    Update bank.js

--- src/Commands/Economy/bank.js
+++ src/Commands/Economy/bank.js
@@ -1,12 +1,13 @@
 const MAX_AMOUNT = 100000000;
 
 module.exports = {
-    name: 'bank',
-    aliases: ['bk'],
+    name: 'treasury',
+    aliases: ['tr'],
     category: 'economy',
     exp: 5,
     cool: 4,
     react: "✅",
+    usage: 'Use :treasury',
     description: 'Shows the bank value of user',
     async execute(client, arg, M) {
         let bank = await client.credit.get(`${M.sender}.bank`) || 0;
@@ -17,13 +18,13 @@ module.exports = {
             M.reply("Bank reached maximum amount; removing extra amount!");
         }
 
-        let text = `🏦 *Bank* 🏦\n\n👤 *Name:* ${(await client.contact.getContact(M.sender, client)).username}\n🔖 *Tag:* #${M.sender.substring(3, 7)}\n💰 *Credits:* ${bank} 🪙`;
+        let text = `🏦 *Bank* 🏦\n\n👤 *Name:* ${(await client.contact.getContact(M.sender, client)).username}\n🔖 *Tag:* #${M.sender.substring(3, 7)}\n💰 *Credits:* ${bank} `;
 
         let imageT = await client.utils.generateCreditCardImage(
             (await client.contact.getContact(M.sender, client)).username,
             '5/25'
         );
 
-        await client.sendMessage(M.from, {image: imageT, caption: text}, {quoted: M});
+        M.reply(text);
     }
 };
