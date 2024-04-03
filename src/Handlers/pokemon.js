9d0a760d5ac0ee5f2d815d0d8b017c3ff51a9184
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-03 22:04:01 +0530

    Update pokemon.js

--- src/Handlers/pokemon.js
+++ src/Handlers/pokemon.js
@@ -1,7 +1,6 @@
 const cron = require("node-cron")
 const axios = require('axios')
 const path = require('path')
-const { calculateRequiredExp } = require('../Helpers/pokeStats')
 require("./Message");
 module.exports = PokeHandler = async (client, m) => {
   try {
@@ -24,7 +23,7 @@ module.exports = PokeHandler = async (client, m) => {
             const types = pokemon.types.map(type => type.type.name);
             const image = pokemon.sprites.other['official-artwork'].front_default;
             const level = Math.floor(Math.random() * (30 - 15) + 15);
-            const requiredExp = calculateRequiredExp(level);
+            const requiredExp = 0;
 
             const pokemonData = { name: name, level: level, exp: requiredExp }; // Create an object with name, level, and exp
            console.log(`Spawned: ${pokemonData.name} in ${jid}`);
