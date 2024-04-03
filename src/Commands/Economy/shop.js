db343d3143c85282368734b0fb40b146ae7ec8fd
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-03 12:32:17 +0530

    Update shop.js

--- src/Commands/Economy/shop.js
+++ src/Commands/Economy/shop.js
@@ -2,11 +2,6 @@ const items = {
     buy: [
         { name: 'luckpotion', price: 1200, usage: 'Increases luck in slotting' },
         { name: 'pepperspray', price: 1000, usage: 'Self-defense against robbers' },
-        { name: 'woodpickaxe', price: 10000, usage: 'Mining in forest' },
-        { name: 'ironpickaxe', price: 30000, usage: 'Mining in iron mine' },
-        { name: 'goldpickaxe', price: 50000, usage: 'Mining in deep forest' },
-        { name: 'diamondpickaxe', price: 70000, usage: 'Mining in coal mine' },
-        { name: 'emeraldpickaxe', price: 100000, usage: 'Mining in top secret mine' },
     ]
 };
 
