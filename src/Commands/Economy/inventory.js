106df8fee1364753a699de2c49436a3cba7b0927
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-04 21:46:02 +0530

    Update inventory.js

--- src/Commands/Economy/inventory.js
+++ src/Commands/Economy/inventory.js
@@ -5,30 +5,33 @@ module.exports = {
     exp: 7,
     cool: 4,
     react: "✅",
-    inevntory: 'Use :inv',
+    usage: 'Use :inv',
     description: 'Gives you details about your inventory',
     async execute(client, arg, M) {
-        const pepper = await client.rpg.get(`${M.sender}.pepperspray`);
-        const luck = await client.rpg.get(`${M.sender}.luckpotion`);
-        const deck = await client.DB.get(`${M.sender}_Deck`) || []
-        const coll = await client.DB.get(`${M.sender}_Collection`) || []
-        const party = await client.DB.get(`${M.sender}_Party`) || []
-        const pc = await client.DB.get(`${M.sender}_PC`) || []
-        const wallet = await client.credit.get(`${M.sender}.wallet`);
-        const bank = await client.credit.get(`${M.sender}.bank`);
-        const pokeballs = await client.rpg.get(`${M.sender}.pokeball`);
-        
-        const inventory = await client.rpg.get(M.sender);
-        if (!inventory) return M.reply('You have no inventory');
-
-        let text = '🗻 *INVENTORY* 🗻\n\n';
-        text += `🌶️ *Pepper Spray:* ${pepper || 0}\n`;
-        text += `🧧 *Luck Potion:* ${luck || 0}\n`;
-        text += `🏀 *Pokeballs:* ${pokeballs || 0}\n;
-        text += `🎴 *Total cards:* ${deck.length + coll.length || 'None'}\n`;
-        text += `🎊 *Total pokemons:* ${party.length + pc.length || 'None'}\n`;
-        text += `💳 *Credits:* ${wallet + bank}`;
-        
-        M.reply(text);
+        try {
+            const pepper = await client.rpg.get(`${M.sender}.pepperspray`) || 0;
+            const luck = await client.rpg.get(`${M.sender}.luckpotion`) || 0;
+            const deck = await client.DB.get(`${M.sender}_Deck`) || [];
+            const coll = await client.DB.get(`${M.sender}_Collection`) || [];
+            const party = await client.DB.get(`${M.sender}_Party`) || [];
+            const pc = await client.DB.get(`${M.sender}_PC`) || [];
+            const wallet = await client.credit.get(`${M.sender}.wallet`) || 0;
+            const bank = await client.credit.get(`${M.sender}.bank`) || 0;
+            const pokeballs = await client.rpg.get(`${M.sender}.pokeball`) || 0;
+            
+            let text = '🗻 *INVENTORY* 🗻\n\n';
+            text += `🌶️ *Pepper Spray:* ${pepper}\n`;
+            text += `🧧 *Luck Potion:* ${luck}\n`;
+            text += `🏀 *Pokeballs:* ${pokeballs}\n`;
+            text += `🎴 *Total cards:* ${deck.length + coll.length || 'None'}\n`;
+            text += `🎊 *Total pokemons:* ${party.length + pc.length || 'None'}\n`;
+            text += `💳 *Credits:* ${wallet + bank}`;
+            
+            M.reply(text);
+        } catch (err) {
+            console.error(err);
+            M.reply("An error occurred while fetching your inventory.");
+        }
     }
 };
+        
