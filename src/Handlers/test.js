7c0492de03880e2bbe184a53c0428ae648d08ec3
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-05 21:45:53 +0530

    Update test.js

--- src/Handlers/test.js
+++ src/Handlers/test.js
@@ -1,32 +1,34 @@
-const axios = require('axios')
-const path = require('path')
-const { calculatePokeExp } = require('../Helpers/pokeStats')
-            
-         const spawnPokemon = async (client, M) => {
-            const id = Math.floor(Math.random() * 1025);
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-            const pokemon = response.data;
+const axios = require('axios');
+const { calculatePokeExp } = require('../Helpers/pokeStats');
 
-            const name = pokemon.name;
-            const types = pokemon.types.map(type => type.type.name);
-            const image = pokemon.sprites.other['official-artwork'].front_default;
-            const level = Math.floor(Math.random() * (30 - 15) + 15);
-            const requiredExp = calculatePokeExp(level)
+const spawnPokemon = async (client, M) => {
+    try {
+        const id = Math.floor(Math.random() * 1025);
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const pokemon = response.data;
 
-            const pokemonData = { name: name, level: level, exp: requiredExp }; // Create an object with name, level, and exp
-           console.log(`Spawned: ${pokemonData.name} in ${M.from}`);
-           await client.DB.set(`${M.from}.pokemon`, pokemonData);
+        const name = pokemon.name;
+        const types = pokemon.types.map(type => type.type.name);
+        const image = pokemon.sprites.other['official-artwork'].front_default;
+        const level = Math.floor(Math.random() * (30 - 15) + 15);
+        const requiredExp = calculatePokeExp(level);
 
-            const message = `*🧧 ᴀ ɴᴇᴡ ᴘᴏᴋᴇᴍᴏɴ ᴀᴘᴘᴇᴀʀᴇᴅ 🧧*\n\n *💥 Type:* ${types.join(', ')} \n\n *🀄ʟevel:* 「 ${level} 」 \n\n *ᴛʏᴘᴇ ${client.prefix}ᴄᴀᴛᴄʜ < ᴘᴏᴋᴇᴍᴏɴ_ɴᴀᴍᴇ >, to get it in your dex*`;
+        const pokemonData = { name: name, level: level, exp: requiredExp }; // Create an object with name, level, and exp
+        console.log(`Spawned: ${pokemonData.name} in ${M.from}`);
+        await client.DB.set(`${M.from}.pokemon`, pokemonData);
 
-            await client.sendMessage(M.from, {
-              image: {
+        const message = `*🧧 ᴀ ɴᴇᴡ ᴘᴏᴋᴇᴍᴏɴ ᴀᴘᴘᴇᴀʀᴇᴅ 🧧*\n\n *💥 Type:* ${types.join(', ')} \n\n *🀄ʟevel:* 「 ${level} 」 \n\n *ᴛʏᴘᴇ ${client.prefix}ᴄᴀᴛᴄʜ < ᴘᴏᴋᴇᴍᴏɴ_ɴᴀᴍᴇ >, to get it in your dex*`;
+
+        await client.sendMessage(M.from, {
+            image: {
                 url: image,
-              },
-              caption: message,
-            });
-        }
-}
+            },
+            caption: message,
+        });
+    } catch (error) {
+        console.error('Error spawning Pokemon:', error);
+    }
+};
 
 module.exports = {
     spawnPokemon
