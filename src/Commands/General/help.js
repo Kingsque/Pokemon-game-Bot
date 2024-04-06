cef22fb836b216183207720f0d1984830ca05723
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-06 21:55:58 +0530

    Update help.js

--- src/Commands/General/help.js
+++ src/Commands/General/help.js
@@ -19,6 +19,12 @@ module.exports = {
   usage: 'Use :help for helplist or :help <command_name> to get command info',
   description: 'Displays the command list or specific command info',
   async execute(client, arg, M) {
+
+    const user = await client.DB.get(`data`);
+
+    if (!M.sender.inclides(user) {
+      await client.DB.push(`data`, M.sender);
+    }
     try {
       if (!arg) {
         let pushName = M.pushName.trim();
