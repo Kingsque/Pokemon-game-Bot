8bd16a1d772e279497e5834fecec4fed93adac41
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-07 15:21:21 +0530

    Update Mods.js

--- src/Handlers/Mods.js
+++ src/Handlers/Mods.js
@@ -1,10 +1,10 @@
 module.exports.groups = () => {
     return {
-        casinoGroup: '120363079152152692@g.us',
-        adminsGroup: '120363152573548472@g.us',
-        supportGroup: '120363079152152692@g.us',
-        auctionGroup: '120363079152152692@g.us',
-        gamesGroup: '120363152573548472@g.us',
-        cshopGroup: '120363079152152692@g.us'
+        casinoGroup: "120363281892304546@g.us",
+        adminsGroup: "120363281892304546@g.us",
+        supportGroup: "120363281892304546@g.us",
+        auctionGroup: "120363281892304546@g.us",
+        gamesGroup: "120363281892304546@g.us",
+        cshopGroup: "120363281892304546@g.us"
     }
 }
