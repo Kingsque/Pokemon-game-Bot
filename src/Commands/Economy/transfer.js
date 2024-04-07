0190c4231c4c71e7b79819f434c78fad5be7c0c6
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-07 15:58:24 +0530

    Update transfer.js

--- src/Commands/Economy/transfer.js
+++ src/Commands/Economy/transfer.js
@@ -8,20 +8,18 @@ module.exports = {
     usage: 'Use :give <amount> @taguser',
     description: 'Transfer golds to your friend',
     async execute(client, arg, M) {
-        if (M.mentions.length === 0) return M.reply('*You must mention someone to send the money*');
+        if (!M.mentions.length) return M.reply('*You must mention someone to send the money*');
         const amount = parseInt(arg.split(' ')[0]);
-        if (isNaN(amount)) return M.reply('Please provide a valid amount');
-        if (amount <= 0) return M.reply('Please provide a positive amount');
-        const credits = (await client.credit.get(`${M.sender}.wallet`)) || 0;
-        if (credits < amount) return M.reply('You don\'t have that much in your wallet');
+        if (isNaN(amount) || amount <= 0) return M.reply('Please provide a valid and positive amount');
+        const senderCredits = (await client.credit.get(`${M.sender}.wallet`)) || 0;
+        if (senderCredits < amount) return M.reply('You don\'t have that much in your wallet');
         await client.credit.add(`${M.mentions[0]}.wallet`, amount);
         await client.credit.sub(`${M.sender}.wallet`, amount);
-        client.sendMessage(
-            M.from,
-            { text: `You gave *${amount}* to *@${M.mentions[0].split('@')[0]}*`, mentions: [M.mentions[0]] },
-            { quoted: M }
-        );
-        let tr = `@${M.sender.split('@')[0]} gave ${amount} to @${M.mentions[0].split('@')[0]}`;
-        await client.sendMessage(client.groups.adminsGroup, tr);
+        const senderName = M.sender.split('@')[0];
+        const recipientName = M.mentions[0].split('@')[0];
+        const messageToSender = `You gave *${amount}* to *@${recipientName}*`;
+        const messageToAdmin = `@${senderName} gave ${amount} to @${recipientName}`;
+        client.sendMessage(M.from, { text: messageToSender, mentions: [M.mentions[0]] }, { quoted: M });
+        await client.sendMessage("120363236615391329@g.us", messageToAdmin);
     }
 };
