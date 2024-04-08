89820fd4e1d761818efd4f6755062c3d5bd66b45
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-08 11:10:12 +0530

    Create test.js

---
+++ src/Commands/Cards/test.js
@@ -0,0 +1,93 @@
+module.exports = {
+  name: 'test',
+  aliases: ['tests'],
+  exp: 0,
+  cool: 4,
+  react: "✅",
+  category: 'card game',
+  usage: 'Use :deck or :deck <index>',
+  description: 'Claim the card',
+  async execute(client, arg, M) {
+    const deck = await client.DB.get(`${M.sender}_Deck`);
+    if (!deck || deck.length === 0) {
+      M.reply('No Deck Found');
+      return;
+    } 
+    try {
+      const maxCardsInDeck = 12;
+      const cardsToMove = deck.slice(maxCardsInDeck);
+      const cardsToKeep = deck.slice(0, maxCardsInDeck);
+      const collection = await client.DB.get(`${M.sender}_Collection`) || [];
+      await client.DB.set(`${M.sender}_Collection`, [...collection, ...cardsToMove]);
+      await client.DB.set(`${M.sender}_Deck`, cardsToKeep);
+      const bgPath = path.join(__dirname, '../../Helpers/bg.json');
+      const bgData = require(bgPath);
+      const backgroundTitle = await client.DB.get(`${M.sender}_BG`);
+
+      let backgroundImageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmru1INQycEtNqouDSnB0XU7_CS3MzEpORvw&usqp=CAU';
+
+      if (backgroundTitle) {
+        const background = bgData.find(bg => bg.Name === backgroundTitle);
+        if (background) {
+          backgroundImageUrl = background.Url;
+        }
+      }
+
+      if (arg) { 
+        const index = parseInt(arg) - 1; // The index in the array is 0-based
+        if (isNaN(index) || index < 0 || index >= deck.length) {
+          M.reply(`Invalid card index. Your deck has ${deck.length} cards.`);
+        } else {
+          const card = deck[index].split('-');
+          const filePath = path.join(__dirname, '../../Helpers/card.json');
+          const data = require(filePath);
+          const cardsInTier = data.filter((cardData) => cardData.tier === card[1]);
+          const cardData = cardsInTier.find((cardData) => cardData.title === card[0]);
+          const cardUrl = cardData.url;
+          let text = `🃏 Total Deck Cards: ${deck.length}\n\n🏮 Username: ${(await client.contact.getContact(M.sender, client)).username} \n*#${index + 1}*\n🃏 *Name:* ${card[0]}\n🪄 *Tier:* ${card[1]} \n`;
+          const file = await client.utils.getBuffer(cardUrl);
+          if (cardUrl.endsWith('.gif')) {
+            const pngBuffer = await gifToPng(file);
+            const pngDataUrl = `data:image/png;base64,${pngBuffer.toString('base64')}`;
+            await client.sendMessage(M.from, {image: {url: pngDataUrl}, caption: text}, {quoted: M});
+          } else {
+            await client.sendMessage(M.from, {image: {url: cardUrl}, caption: text}, {quoted: M});
+          }
+        }
+      } else {
+        const images = [];
+        let cardText = "";
+        const cardSet = new Set();
+        for (let i = 0; i < deck.length; i++) {
+          const card = deck[i].split('-');
+          const filePath = path.join(__dirname, '../../Helpers/card.json');
+          const data = require(filePath);
+          const cardsInTier = data.filter((cardData) => cardData.tier === card[1]);
+          const cardData = cardsInTier.find((cardData) => cardData.title === card[0]);
+          const cardKey = `${cardData.title}-${card[1]}-${cardData.url}-${i}`
+          let cardUrl = cardData.url;
+          if (cardUrl.endsWith('.gif')) {
+            const pngBuffer = await gifToPng(await client.utils.getBuffer(cardUrl));
+            cardUrl = `data:image/png;base64,${pngBuffer.toString('base64')}`;
+          }
+          if (!cardSet.has(cardKey)) {
+            cardSet.add(cardKey);
+            images.push(cardUrl);
+          }
+          cardText += `🔰Card ${i+1}:\n🌟Tier: ${card[1]}\n💎Name ${card[0]}\n\n`;
+        }
+        
+        const canvasWidth = 1050;
+        const canvasHeight = 1800;
+        const canvas = createCanvas(canvasWidth, canvasHeight);
+        const ctx = canvas.getContext('2d');
+        const backgroundImage = await loadImage(backgroundImageUrl);
+        ctx.drawImage(backgroundImage, 0, 0, canvasWidth, canvasHeight);
+        const imageWidth = 350;
+        const imageHeight = 450;
+        const imagePadding = 10;
+        const imagesPerRow = 3;
+        const rows = 4;
+        const xStart = (canvasWidth - (imageWidth * imagesPerRow + imagePadding * (imagesPerRow - 1))) / 2;
+        const yStart = (canvasHeight - (imageHeight * rows + imagePadding * (rows - 1))) / 2
+        
