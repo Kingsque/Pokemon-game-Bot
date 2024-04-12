280ad8898b4035fa9b84c2d1b2a95869f03ccc1e
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-12 16:07:01 +0530

    Update wallet.js

--- src/Commands/Economy/wallet.js
+++ src/Commands/Economy/wallet.js
@@ -8,7 +8,18 @@ module.exports = {
     usage: 'Use :credit',
     description: 'Shows the wallet value',
     async execute(client, arg, M) {
-        const wallet = await client.credit.get(`${M.sender}.wallet`) || 0;
+        let wallet = await client.credit.get(`${M.sender}.wallet`) || 0;
+        
+        // Check if the amount starts with a "-" sign
+        if (wallet.toString().startsWith('-')) {
+            wallet = 0; // Convert negative amount to 0
+        }
+        
+        // Check if the amount contains a "." sign
+        if (wallet.toString().includes('.')) {
+            wallet = Math.round(wallet); // Round decimal or fraction amounts
+        }
+        
         const contact = await client.contact.getContact(M.sender, client);
         const username = contact.username || 'Unknown';
         const tag = `#${M.sender.substring(3, 7)}`;
