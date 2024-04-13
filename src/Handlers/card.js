d66016e11cb503af94630fb1fa02de7343f0b0b6
Rusheil Singh Baath <144768654+rushei1@users.noreply.github.com>
2024-04-13 17:58:54 +0530

    Update card.js

--- src/Handlers/card.js
+++ src/Handlers/card.js
@@ -1,5 +1,4 @@
 const cron = require("node-cron");
-const axios = require('axios');
 const path = require('path');
 require("./Message");
 
@@ -11,102 +10,70 @@ module.exports = CardHandler = async (client, m) => {
     if (cardgame.length > 0) {
       const randomIndex = Math.floor(Math.random() * cardgame.length);
       const randomJid = cardgame[randomIndex];
-      let jid = randomJid;
-      console.log(jid);
+      const jid = randomJid;
 
       if (cardgame.includes(jid)) {
+        const filePath = path.join(__dirname, '../Helpers/spawn.json');
+        const data = require(filePath);
 
-        let count = 0;
-        let sOr6Counter = 0;
-        const sOr6Interval = 10;
-        const sOr6Limit = 100;
+        const index = Math.floor(Math.random() * data.length);
+        let obj, price;
 
-        cron.schedule('*/16 * * * *', async () => {
-          try {
-            const filePath = path.join(__dirname, '../Helpers/spawn.json');
-            const data = require(filePath);
+        obj = data[index];
+        switch (obj.tier) {
+          case "1":
+            price = client.utils.getRandomInt(1000, 2000);
+            break;
+          case "2":
+            price = client.utils.getRandomInt(2000, 3000);
+            break;
+          case "3":
+            price = client.utils.getRandomInt(3000, 5000);
+            break;
+          case "4":
+            price = client.utils.getRandomInt(5000, 8000);
+            break;
+          case "5":
+            price = client.utils.getRandomInt(15000, 20000);
+            break;
+          case "6":
+            price = client.utils.getRandomInt(30000, 60000);
+            break;
+          case "S":
+            price = client.utils.getRandomInt(60000, 100000);
+            break;
+        }
+        const code = client.utils.getRandomInt(11111, 99999);
 
-            const index = Math.floor(Math.random() * data.length);
-            let obj, price;
+        console.log(`Sended:${obj.tier}  Name:${obj.title}  For ${price} in ${jid}`);
 
-            obj = data[index];
-            switch (obj.tier) {
-              case "1":
-                price = client.utils.getRandomInt(1000, 2000);
-                break;
-              case "2":
-                price = client.utils.getRandomInt(2000, 3000);
-                break;
-              case "3":
-                price = client.utils.getRandomInt(3000, 5000);
-                break;
-              case "4":
-                price = client.utils.getRandomInt(5000, 8000);
-                break;
-              case "5":
-                price = client.utils.getRandomInt(15000, 20000);
-                break;
-              case "6":
-                price = client.utils.getRandomInt(30000, 60000);
-                break;
-              case "S":
-                price = client.utils.getRandomInt(60000, 100000);
-                break;
-            }
-            const code = client.utils.getRandomInt(11111, 99999);
+        // Setting card details including code for all tiers
+        await client.cards.set(`${jid}.card`, `${obj.title}-${obj.tier}`);
+        await client.cards.set(`${jid}.cardPrice`, price);
+        await client.cards.set(`${jid}.code`, code);
 
-            count++;
-            sOr6Counter++;
-            if (sOr6Counter === sOr6Interval && sOr6Counter <= (sOr6Interval * sOr6Limit)) {
-              const filteredData = data.filter(card => card.tier === "S" || card.tier === "6");
-              const index = Math.floor(Math.random() * filteredData.length);
-              obj = filteredData[index];
-              switch (obj.tier) {
-                case "6":
-                  price = client.utils.getRandomInt(30000, 60000);
-                  break;
-                case "S":
-                  price = client.utils.getRandomInt(60000, 100000);
-                  break;
-              }
-            }
-
-            console.log(`Sended:${obj.tier + "  Name:" + obj.title + "  For " + price + " in " + jid}`);
-            
-            // Setting card details including code for all tiers
-            await client.cards.set(`${jid}.card`, `${obj.title}-${obj.tier}`);
-            await client.cards.set(`${jid}.cardPrice`, price);
-            await client.cards.set(`${jid}.code`, code);
-
-            if (obj.tier.includes('6') || obj.tier.includes('S')) {
-              const giif = await client.utils.getBuffer(obj.url);
-              const cgif = await client.utils.gifToMp4(giif);
-              await client.sendMessage(jid, {
-                video: cgif,
-                caption: `*━『  🎊Finally a rare card has spawned🎊 』━*\n\n🎴 *Name:* ${obj.title}\n\n🎐 *Tier:* ${obj.tier}\n\n🪩 *Price:* ${price}\n\n🎴 *code:* ${code}\n\n🔖 Use *${client.prefix}collect <code>* to claim the card, your card will be stored in you deck`,
-                gifPlayback: true,
-              });
-            } else {
-              await client.sendMessage(jid, {
-                image: {
-                  url: obj.url,
-                },
-                caption: `*━『 🎊A new card has spawned🎊 』━*\n\n🎴 *Name:* ${obj.title}\n\n🎐 *Tier:* ${obj.tier}\n\n🪩 *Price:* ${price}\n\n🎴 *code:* ${code}\n\n🔖 Use *${client.prefix}collect <code>* to claim the card, your card will be stored in you deck`,
-              });
-            }
-
-            cron.schedule('*/15 * * * *', async () => {
-              await client.cards.delete(`${jid}.card`);
-              console.log(`Card deleted after 5 minutes`);
-            }, { scheduled: false, timezone: "Asia/Tokyo" });
-
-          } catch (err) {
-            console.log(err);
-            await client.sendMessage(jid, { image: { url: `${client.utils.errorChan()}` }, caption: `${client.utils.greetings()} Error-Chan Dis\n\nCommand no error wa:\n${err}` });
-          }
-        }, { scheduled: false, timezone: "Asia/Tokyo" });
+        if (obj.tier.includes('6') || obj.tier.includes('S')) {
+          const giif = await client.utils.getBuffer(obj.url);
+          const cgif = await client.utils.gifToMp4(giif);
+          await client.sendMessage(jid, {
+            video: cgif,
+            caption: `*━『  🎊Finally a rare card has spawned🎊 』━*\n\n🎴 *Name:* ${obj.title}\n\n🎐 *Tier:* ${obj.tier}\n\n🪩 *Price:* ${price}\n\n🎴 *code:* ${code}\n\n🔖 Use *${client.prefix}collect <code>* to claim the card, your card will be stored in you deck`,
+            gifPlayback: true,
+          });
+        } else {
+          await client.sendMessage(jid, {
+            image: {
+              url: obj.url,
+            },
+            caption: `*━『 🎊A new card has spawned🎊 』━*\n\n🎴 *Name:* ${obj.title}\n\n🎐 *Tier:* ${obj.tier}\n\n🪩 *Price:* ${price}\n\n🎴 *code:* ${code}\n\n🔖 Use *${client.prefix}collect <code>* to claim the card, your card will be stored in you deck`,
+          });
+        }
       }
     }
+
+    cron.schedule('*/16 * * * *', async () => {
+      // Implement card expiration logic here if needed
+    }, { scheduled: false, timezone: "Asia/Tokyo" });
 
   } catch (error) {
     console.log(error);
