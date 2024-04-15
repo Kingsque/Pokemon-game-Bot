4610666da36aa01497fa2bf6b60538906baebc52
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-15 20:39:50 +0530

    Update deposit.js

--- src/Commands/Economy/deposit.js
+++ src/Commands/Economy/deposit.js
@@ -1,3 +1,4 @@
+// Deposit Command
 module.exports = {
     name: 'deposit',
     aliases: ["dt", "depo"],
@@ -8,14 +9,12 @@ module.exports = {
     usage: 'Use :deposit <amount>',
     description: 'Deposits credits from your credits to your treasury',
     async execute(client, arg, M) {
-    
-        if (!arg || isNaN(arg)) return M.reply('Please provide a valid amount');
+        if (!arg || isNaN(arg)) return M.reply('Please provide a valid amount.');
+        
         const amount = parseInt(arg);
-        if (amount <= 0) return M.reply('Please provide a positive amount');
+        if (amount <= 0) return M.reply('Please provide a positive amount.');
+
         const wallet = (await client.credit.get(`${M.sender}.wallet`)) || 0;
-        if (wallet < amount) return M.reply('You don\'t have enough in your wallet');
-        await client.credit.add(`${M.sender}.bank`, amount);
-        await client.credit.sub(`${M.sender}.wallet`, amount);
-        M.reply(`You have successfully deposited ${amount} in your bank`);
-    }
-};
+        if (wallet < amount) return M.reply('You don\'t have enough credits in your wallet.');
+
+        await client.credit.add(`${M.sender}.bank
