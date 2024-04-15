fa6e76cae6e398811a8ef47e174149d8724fae61
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-15 21:23:42 +0530

    Update transfer.js

--- src/Commands/Economy/transfer.js
+++ src/Commands/Economy/transfer.js
@@ -8,7 +8,13 @@ module.exports = {
     usage: 'Use :give <amount> @taguser',
     description: 'Transfer credits to your friend',
     async execute(client, arg, M) {
-        if (!M.mentions.length) return M.reply('You must mention someone to transfer credits to.');
+        const groupMetadata = await client.groupMetadata(M.from);
+        const groupMembers = groupMetadata?.participants || [];
+        const groupAdmins = groupMembers.filter((v) => v.isAdmin).map((v) => v.id);
+        
+        const give = M.mentions[0] || (M.quoted && M.quoted.participant);
+
+        if (!give) return M.reply('You must mention someone to transfer credits to.');
 
         const amount = parseInt(arg.split(' ')[0]);
         if (isNaN(amount) || amount <= 0) return M.reply('Please provide a valid positive amount.');
@@ -16,21 +22,19 @@ module.exports = {
         const senderCredits = (await client.credit.get(`${M.sender}.wallet`)) || 0;
         if (senderCredits < amount) return M.reply('You don\'t have enough credits in your wallet.');
 
-        const recipientWallet = await client.credit.get(`${M.mentions[0]}.wallet`);
+        const recipientWallet = await client.credit.get(`${give}.wallet`);
         const recipientCredits = recipientWallet ? recipientWallet : 0;
 
-        await client.credit.add(`${M.mentions[0]}.wallet`, amount);
+        await client.credit.add(`${give}.wallet`, amount);
         await client.credit.sub(`${M.sender}.wallet`, amount);
 
         const senderName = M.sender.split('@')[0];
-        const recipientName = M.mentions[0].split('@')[0];
+        const recipientName = give.split('@')[0];
 
-        const messageToSender = `You gave *${amount}* credits to *@${recipientName}*`;
-        const messageToRecipient = `You received *${amount}* credits from *@${senderName}*`;
         const messageToAdmin = `@${senderName} gave ${amount} credits to @${recipientName}`;
+        const message = `You gave *${amount}* credits to *@${recipientName}*`;
 
-        await client.sendMessage(M.from, { text: messageToSender, mentions: [M.mentions[0]] });
-        await client.sendMessage(M.mentions[0], { text: messageToRecipient, mentions: [M.sender] });
+        await client.sendMessage(M.from, { text: message, mentions: [give] });
         await client.sendMessage("120363236615391329@g.us", { text: messageToAdmin });
     }
 };
