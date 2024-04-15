d4b828d01cec5b2b93904af3be998d57349189e0
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-15 20:48:37 +0530

    Update slot.js

--- src/Commands/Economy/slot.js
+++ src/Commands/Economy/slot.js
@@ -10,7 +10,6 @@ module.exports = {
     usage: 'Use: !slot <amount>',
     description: 'Bets the given amount of credits in a slot machine',
     async execute(client, arg, M) {
-
         const symbols = [
             new SlotSymbol('a', {
                 display: '🍉',
@@ -34,20 +33,23 @@ module.exports = {
             }),
         ];
 
-        if (!arg) return M.reply('Please provide the amount');
+        if (!arg) return M.reply('Please provide the amount.');
+        
         const amount = parseInt(arg);
 
-        if (isNaN(amount) || amount <= 0) return M.reply('Please provide a valid amount');
-        if (arg.startsWith('-') || arg.startsWith('+')) return M.reply('Please provide a valid amount');
+        if (isNaN(amount) || amount <= 0) return M.reply('Please provide a valid positive amount.');
+        
+        if (arg.startsWith('-') || arg.startsWith('+')) return M.reply('Please provide a valid amount.');
 
         const credits = (await client.credit.get(`${M.sender}.wallet`)) || 0;
 
-        if (amount > credits) return M.reply("You don't have sufficient funds");
-        if (amount > 20000) return M.reply('You cannot bet more than 20000 credits in the slot machine');
-        if (amount < 500) return M.reply('You cannot bet less than 500 credits in the slot machine');
+        if (amount > credits) return M.reply("You don't have sufficient funds.");
+        
+        if (amount > 20000) return M.reply('You cannot bet more than 20000 credits in the slot machine.');
+        
+        if (amount < 500) return M.reply('You cannot bet less than 500 credits in the slot machine.');
 
         const machine = new SlotMachine(3, symbols).play();
-        const lines = machine.lines.filter((line) => !line.diagonal);
         const points = machine.lines.reduce((total, line) => total + line.points, 0);
 
         let resultAmount = points <= 0 ? -amount : amount * points;
@@ -56,7 +58,7 @@ module.exports = {
         const isJackpotTriggered = jackpotChance < 0.05; // Define jackpot triggering probability
 
         if (isJackpotTriggered) {
-            const jackpotWin = amount * 200000; // Update jackpot win amount
+            const jackpotWin = 200000; // Update jackpot win amount
             await client.credit.add(`${M.sender}.wallet`, jackpotWin);
             return M.reply(`🎰 *SLOT MACHINE* 🎰\n 🍉 🍉 🍉\n🍉 🍉 🍉\n🍉 🍉 🍉 \nCongratulations! You hit the jackpot and won ${jackpotWin} credits!`);
         } else {
@@ -71,6 +73,9 @@ module.exports = {
             if (points > 0 || Math.random() < luckFactor) { // Introduce luck probability here
                 resultAmount *= luckFactor; // Multiply result amount by luck factor
             }
+            
+            resultAmount = Math.round(resultAmount);
+            if (resultAmount > 150000) resultAmount = 150000;
 
             await client.credit.add(`${M.sender}.wallet`, resultAmount);
 
@@ -82,6 +87,7 @@ module.exports = {
                 await client.rpg.sub(`${M.sender}.luckpotion`, 1);
                 text += '\n\n🍀 You have been saved by your luck potion!';
             } else {
+                if (points <= 0) await client.credit.sub(`${M.sender}.wallet`, amount);
                 text += points <= 0 ? `\n\n📉 You lost ${amount} credits` : `\n\n📈 You won ${resultAmount} credits`;
             }
 
