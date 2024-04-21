d0759f536ab36e0841ec48ef9ba5de6e887f66d9
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-21 16:21:35 +0000

    ntg

--- src/Commands/Pokemon/partymon.js
+++ src/Commands/Pokemon/partymon.js
@@ -1,12 +1,12 @@
 const { Sets } = require('@pkmn/sets');
 const { Screens } = require('pkmn-screens');
 const { summaryScreen, partyScreen } = require('@shineiichijo/team-preview');
-const { requirePokeExpToLevelUp } = require('../../Helpers/pokeStats');
+const { calculatePokeExp } = require('../../Helpers/pokeStats');
 
 module.exports = {
-    name: "party",
-    aliases: ["party"],
-    exp:4,
+    name: "test",
+    aliases: ["t"],
+    exp: 4,
     cool: 5,
     react: "🟩",
     category: "pokemon",
@@ -25,11 +25,12 @@ module.exports = {
                     return M.reply("Invalid index. Please provide a valid index within your party range.");
                 }
                 const pokemon = party[index - 1];
-                const neededExp = requirePokeExpToLevelUp(pokemon.exp, pokemon.level);
+                const neededExp = pokemon.level + 1;
+                const need = calculatePokeExp(neededExp);
                 let text = `🟩 *Name:* ${client.utils.capitalize(pokemon.name)}
-                \n\n🟧 *Types:* ${pokemon.types.map(client.utils.capitalize).join(', ')}\n\n🟨 *Level:* ${
+                \n\n🟧 *Types:* ${pokemon.type.map(client.utils.capitalize).join(', ')}\n\n🟨 *Level:* ${
                     pokemon.level
-                }\n\n🟦 *XP:* ${pokemon.pokexp} / ${neededExp}
+                }\n\n🟦 *XP:* ${pokemon.pokexp} / ${need}
                 \n\n🟢 *HP:* ${pokemon.hp} / ${pokemon.maxHp}\n\n⬜ *Speed:* ${pokemon.speed} / ${
                     pokemon.maxSpeed
                 }\n\n🛡 *Defense:* ${pokemon.defense} / ${pokemon.maxDefense}\n\n🟥 *Attack:* ${pokemon.attack} / ${
@@ -40,6 +41,47 @@ module.exports = {
                     index + 1
                 } --moves to see all of the moves of the pokemon with details]*`;
                 M.reply(text);
+
+                if (arg.includes('--moves')) {
+                    const moves = pokemon.moves.map(move => ({
+                        name: move.name,
+                        pp: move.pp,
+                        maxPp: move.maxPp,
+                        type: move.type
+                    }));
+                    const buffer = await client.utils.gifToMp4(
+                        await summaryScreen({
+                            pokemon: { name: pokemon.name, moves, level: pokemon.level, female: pokemon.female },
+                            pokeball: 'pokeball'
+                        })
+                    );
+
+                    let texto = `*Moves | ${client.utils.capitalize(pokemon.name)}*`;
+                    for (let i = 0; i < pokemon.moves.length; i++) {
+                        texto += `\n\n*#${i + 1}*\n❓ *Move:* ${pokemon.moves[i].name
+                            .split('-')
+                            .map(client.utils.capitalize)
+                            .join(' ')}\n〽 *PP:* ${pokemon.moves[i].pp} / ${
+                            pokemon.moves[i].maxPp
+                        }\n🎗 *Type:* ${client.utils.capitalize(pokemon.moves[i].type)}\n🎃 *Power:* ${
+                            pokemon.moves[i].power
+                        }\n🎐 *Accuracy:* ${pokemon.moves[i].accuracy}\n🧧 *Description:* ${
+                            pokemon.moves[i].description
+                        }`;
+                    }
+
+                    client.sendMessage(
+                        M.from,
+                        {
+                            video: buffer,
+                            caption: texto,
+                            gifPlayback: true
+                        },
+                        {
+                            quoted: M
+                        }
+                    );
+                }
             } else {
                 const teamData = party.map(pokemon => ({
                     name: pokemon.name,
