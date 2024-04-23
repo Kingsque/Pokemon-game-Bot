fa71c3965f755ced82d10b9305e330d48a54a165
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-23 14:12:03 +0530

    Update card.js

--- src/Database/Models/card.js
+++ src/Database/Models/card.js
@@ -4,7 +4,8 @@ const cardSchema = new mongoose.Schema({
   jid: String,
   Getcard: String,
   claimed: String,
-  card_price: Number
+  card_price: Number,
+  card_code: Number
 });
 
 const Card = mongoose.model('Card', cardSchema);
