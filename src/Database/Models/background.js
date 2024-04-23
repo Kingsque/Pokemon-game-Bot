36c07a3f059b589213295bcf1922754ccc1437b3
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-23 13:55:06 +0530

    Delete src/Database/Models/background.js

--- src/Database/Models/background.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const backgroundSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  background: { type: [Object], default: [] },
-});
-
-module.exports = mongoose.model("UserBackground", backgroundSchema);
