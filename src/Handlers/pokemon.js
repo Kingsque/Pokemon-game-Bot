4212e2805e49058bd68c102236f1147cb8dafbfe
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-24 14:38:33 +0530

    Update pokemon.js

--- src/Handlers/pokemon.js
+++ src/Handlers/pokemon.js
@@ -1,12 +1,11 @@
-const cron = require("node-cron");
-const axios = require('axios');
-const path = require('path');
-const { calculatePokeExp } = require('../Helpers/pokeStats');
+const cron = require("node-cron")
+const axios = require('axios')
+const path = require('path')
+const { calculateRequiredExp } = require('../Helpers/pokeStats')
 require("./Message");
-
 module.exports = PokeHandler = async (client, m) => {
   try {
-    let wilds = await client.DB.get('wild');
+      let wilds = await client.DB.get('wild');
     const wild = wilds || [];
 
     if (wild.length > 0) {
@@ -17,59 +16,21 @@ module.exports = PokeHandler = async (client, m) => {
       if (wild.includes(jid)) {
         cron.schedule('*/20 * * * *', async () => {
           try {
-            const id = Math.floor(Math.random() * 898) // Ensure ID is within valid range
+            const id = Math.floor(Math.random() * 1025);
             const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
             const pokemon = response.data;
 
             const name = pokemon.name;
             const types = pokemon.types.map(type => type.type.name);
             const image = pokemon.sprites.other['official-artwork'].front_default;
-            const level = Math.floor(Math.random() * (10 - 5) + 5);
-            const requiredExp = calculatePokeExp(level);
-            
-            // Extracting base stats
-            const baseStats = {};
-            pokemon.stats.forEach(stat => {
-              baseStats[stat.stat.name] = stat.base_stat;
-            });
+            const level = Math.floor(Math.random() * (30 - 15) + 15);
+            const requiredExp = calculateRequiredExp(level);
 
-            const dataResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-            const data = await dataResponse.json();
-            const moves = data.moves.slice(0, 2); // 
-            const movesDetails = await Promise.all(moves.map(async move => {
-              const moveName = move.move.name;
-              const moveUrl = move.move.url;
-              const moveDataResponse = await fetch(moveUrl);
-              const moveData = await moveDataResponse.json();
-              const movePower = moveData.power || 0;
-              const moveAccuracy = moveData.accuracy || 0;
-              const movePP = moveData.pp || 5;
-              const moveType = moveData.type ? moveData.type.name : 'Normal';
-              const moveDescription = moveData.flavor_text_entries.find(entry => entry.language.name === 'en').flavor_text;
-              return { name: moveName, power: movePower, accuracy: moveAccuracy, pp: movePP, type: moveType, description: moveDescription };
-            }));
+            const pokemonData = { name: name, level: level, exp: requiredExp }; // Create an object with name, level, and exp
+           console.log(`Spawned: ${pokemonData.name} in ${jid}`);
+           await client.DB.set(`${jid}.pokemon`, pokemonData);
 
-            
-            const pokemonData = { 
-              name: name, 
-              level: level, 
-              pokexp: requiredExp,
-              id: pokemon.id,
-              hp: baseStats['hp'] - 20,
-              maxHp: baseStats['hp'],
-              maxAttack: baseStats['attack'],
-              maxDefense: baseStats['defense'],
-              maxSpeed: baseStats['speed'],
-              type: types,
-              moves: movesDetails,
-              status: '',
-              movesUsed: 0
-            };
-
-            console.log(`Spawned: ${pokemonData.name} in ${jid}`);
-            await client.DB.set(`${jid}.pokemon`, pokemonData);
-
-            const message = `*🧧 ᴀ ɴᴇᴡ ᴘᴏᴋᴇᴍᴏɴ ᴀᴘᴘᴇᴀʀᴇᴅ 🧧*\n\n *💥 Types:* ${types.join(', ')} \n\n *🀄ʟevel:* 「 ${level} 」 \n\n *Available Moves:* ${movesDetails.map(move => move.name).join(', ')} \n\n *ᴛʏᴘᴇ ${client.prefix}ᴄᴀᴛᴄʜ < ᴘᴏᴋᴇᴍᴏɴ_ɴᴀᴍᴇ >, to get it in your dex*`;
+            const message = `*🧧 ᴀ ɴᴇᴡ ᴘᴏᴋᴇᴍᴏɴ ᴀᴘᴘᴇᴀʀᴇᴅ 🧧*\n\n *💥 Type:* ${types.join(', ')} \n\n *🀄ʟevel:* 「 ${level} 」 \n\n *ᴛʏᴘᴇ ${client.prefix}ᴄᴀᴛᴄʜ < ᴘᴏᴋᴇᴍᴏɴ_ɴᴀᴍᴇ >, to get it in your dex*`;
 
             await client.sendMessage(jid, {
               image: {
@@ -79,21 +40,24 @@ module.exports = PokeHandler = async (client, m) => {
             });
           } catch (err) {
             console.log(err);
-            await client.sendMessage(M.from, {
-              text: `Error occurred while spawning Pokémon: ${err.message}`
+            await client.sendMessage(jid, {
+              text: `Error occurred while spawning Pokémon.`
             });
           }      
   
-          cron.schedule('*/15 * * * *', async () => {
-            await client.DB.delete(`${jid}.pokemon`);
-            console.log(`Pokemon deleted after 15 minutes`);
-          });
+    cron.schedule('*/15 * * * *', async () => {
+     await client.DB.delete(`${jid}.pokemon`);
+      console.log(`Pokemon deleted after 5minutes`)
   
-        });
-      }
-    }
-  } catch(error) {
-    console.log(error);
+    })
+  
+  });
+  
   }
-};
+    }
     
+    } catch(error){
+        console.log(error)
+    }
+
+}
