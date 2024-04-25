b8368b31cd0830e05143b5569b5a9a8d2b2a448f
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-25 12:09:32 +0530

    Update collect.js

--- src/Commands/Cards/collect.js
+++ src/Commands/Cards/collect.js
@@ -9,11 +9,12 @@ module.exports = {
   description: "Claim the card that is spawned",
   async execute(client, arg, M) {
     try {
-      const card = await client.cards.get(`${M.from}.card`);
-      const cardPrice = await client.cards.get(`${M.from}.card_price`);
+      const card = client.cardMap.get(M.from);
       if (!card) {
         return M.reply("🙅‍♀️ Sorry, there are currently no available cards to claim!");
       }
+
+      const cardPrice = card.price;
 
       const deck = await client.DB.get(`${M.sender}_Deck`) || [];
       const collection = await client.DB.get(`${M.sender}_Collection`) || [];
@@ -30,15 +31,15 @@ module.exports = {
       // Deduct the card price from the user's wallet
       await client.credit.sub(`${M.sender}.wallet`, cardPrice);
 
-      const [title, tier] = card.split("-");
+      const [title, tier] = card.card.split("-");
 
       let text = `🃏 ${title} (${tier}) has been safely stored in your deck!`;
 
       if (deck.length < 12) {
-        deck.push(card);
+        deck.push(card.card);
       } else {
         text = `🃏 ${title} (${tier}) has been safely stored in your collection!`;
-        collection.push(card);
+        collection.push(card.card);
       }
 
       await client.DB.set(`${M.sender}_Deck`, deck);
@@ -48,8 +49,7 @@ module.exports = {
         `🎉 You have successfully claimed *${title} - ${tier}* for *${cardPrice} Credits* ${text}`
       );
 
-      await client.cards.delete(`${M.from}.card`);
-      await client.cards.delete(`${M.from}.card_price`);
+      client.cardMap.delete(M.from);
     } catch (err) {
       await client.sendMessage(M.from, {
         image: { url: `${client.utils.errorChan()}` },
@@ -58,3 +58,4 @@ module.exports = {
     }
   },
 };
+        
