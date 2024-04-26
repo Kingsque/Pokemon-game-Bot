5c6e08adfaf206f3a1cfc25530381efd0944f9ec
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-26 14:45:01 +0530

    Update create.js

--- src/Commands/General/create.js
+++ src/Commands/General/create.js
@@ -1,3 +1,5 @@
+const { drawHangMan } = require('./your/drawHangMan/module/path');
+
 module.exports = {
     name: 'create',
     aliases: ['create'],
@@ -8,10 +10,19 @@ module.exports = {
     usage: 'Use :info',
     description: 'Get bot information',
     async execute(client, arg, M) {
-        if (!arg) return M.reply('Use :create ttt/hm');
-        if (arg === 'ttt') {
-            const buffer = await client.utils.drawTTTBoard()
-            await client.sendMessage(M.from , {image: buffer , caption: `Your ttt`} , {quoted: M})
+        if (!arg || arg.length < 1) return M.reply('Use :create ttt/hm');
+
+        const [gameType] = arg;
+
+        if (gameType === 'ttt') {
+            const buffer = await client.utils.drawTTTBoard();
+            await client.sendMessage(M.from, { image: buffer, caption: `Your ttt` }, { quoted: M });
+        } else if (gameType === 'hm') {
+            const mistakes = Math.floor(Math.random() * 6) + 1; // Generate random mistakes from 1 to 6
+            const buffer = await drawHangMan(mistakes);
+            await client.sendMessage(M.from, { image: buffer, caption: `Hangman with ${mistakes} mistake(s)` }, { quoted: M });
+        } else {
+            M.reply('Invalid game type. Use :create ttt/hm');
         }
     }
 };
