28588842d110f9b4614a0598183493e95eeed8dc
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-29 22:13:20 +0530

    Update withdraw.js

--- src/Commands/Economy/withdraw.js
+++ src/Commands/Economy/withdraw.js
@@ -14,11 +14,11 @@ module.exports = {
         const amount = parseInt(arg);
         if (amount <= 0) return M.reply('Please provide a positive amount.');
 
-        const bank = (await client.credit.get(`${M.sender}.bank`)) || 0;
+        const bank = (await client.gem.get(`${M.sender}.bank`)) || 0;
         if (bank < amount) return M.reply('You don\'t have enough credits in your bank.');
 
-        await client.credit.add(`${M.sender}.wallet`, amount);
-        await client.credit.sub(`${M.sender}.bank`, amount);
+        await client.gem.add(`${M.sender}.wallet`, amount);
+        await client.gem.sub(`${M.sender}.bank`, amount);
 
         M.reply(`You have successfully withdrawn ${amount} credits from your bank.`);
     }
