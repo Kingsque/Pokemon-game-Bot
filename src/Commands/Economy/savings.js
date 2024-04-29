fa1577b6594dfd16f0607220ae8cc941f6af420f
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-29 22:10:28 +0530

    Update savings.js

--- src/Commands/Economy/savings.js
+++ src/Commands/Economy/savings.js
@@ -1,3 +1,4 @@
+// Savings Command
 const MAX_AMOUNT = 100000000;
 
 module.exports = {
@@ -10,23 +11,23 @@ module.exports = {
     usage: 'Use :savings',
     description: 'Shows the savings value of user',
     async execute(client, arg, M) {
-        let bank = await client.credit.get(`${M.sender}.bank`) || 0;
+        let bank = await client.gem.get(`${M.sender}.bank`) || 0;
 
         // Convert negative amount to 0
         if (bank < 0) {
             bank = 0;
-            await client.credit.set(`${M.sender}.bank`, bank);
+            await client.gem.set(`${M.sender}.bank`, bank);
         }
 
         // Convert decimal or fraction amounts to nearest integer
         if (!Number.isInteger(bank)) {
             bank = Math.round(bank);
-            await client.credit.set(`${M.sender}.bank`, bank);
+            await client.gem.set(`${M.sender}.bank`, bank);
         }
 
         if (bank > MAX_AMOUNT) {
             bank = MAX_AMOUNT;
-            await client.credit.set(`${M.sender}.bank`, bank);
+            await client.gem.set(`${M.sender}.bank`, bank);
             M.reply("Bank reached maximum amount; removing extra amount!");
         }
 
