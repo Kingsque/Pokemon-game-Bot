1bc3f6f2a193e92d6ba7613fe7b16f9b8087f0e7
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-30 15:16:11 +0530

    Update treasury.js

--- src/Commands/Economy/treasury.js
+++ src/Commands/Economy/treasury.js
@@ -10,24 +10,15 @@ module.exports = {
     description: 'Shows the treasury value of user',
     async execute(client, arg, M) {
         const userId = M.sender;
-        let treasury = 0;
 
         const economy = await client.econ.findOne({ userId });
 
-        if (economy) {
-            treasury = economy.treasury;
-        } else {
-            const newEconomy = new client.econ({ userId, treasury: 0 }); // Initialize treasury to 0
-            await newEconomy.save();
-            await M.reply("Welcome to the economy system! 🎉 Congratulations on creating your account! Your treasury has been initialized. 💰");
-        }
+        const treasury = economy.treasury;
 
         const contact = await client.contact.getContact(M.sender, client);
         const username = contact.username || 'Unknown';
         const tag = `#${M.sender.substring(3, 7)}`;
         const thumbnail = await client.utils.getBuffer('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRBfcfZ3LyY8EzPbH5LbHYOxOW0p7Ki5aIenqTSFm5YQ&s.jpg');
-
-        await economy.save(); // Save the updated economy object
 
         await client.sendMessage(M.from, {
             text: "",
