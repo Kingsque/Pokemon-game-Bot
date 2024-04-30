c861458e054456842231b0b85e452152b13c769f
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-30 14:38:59 +0530

    Update buy.js

--- src/Commands/Economy/buy.js
+++ src/Commands/Economy/buy.js
@@ -24,16 +24,17 @@ module.exports = {
         if (!items[itemName]) return M.reply('Please provide a valid item name.');
 
         const totalPrice = items[itemName] * quantity;
-        const userWallet = (await client.gem.get(`${M.sender}.wallet`)) || 0;
+        const userWallet = (await client.econ.findOne({ userId: M.sender }))?.gem || 0;
 
         if (userWallet < totalPrice) {
             return M.reply(`You don't have enough credits to buy ${quantity} ${itemName}(s).`);
         }
 
-        await client.gem.sub(`${M.sender}.wallet`, totalPrice);
-        await client.rpg.add(`${M.sender}.${itemName}`, quantity);
+        const economy = await client.econ.findOne({ userId: M.sender });
+        economy.gem -= totalPrice;
+        economy[itemName] += quantity;
+        await economy.save();
 
-        const newItemAmount = (await client.rpg.get(`${M.sender}.${itemName}`)) || 0;
-        M.reply(`Thank you for your purchase! You now have ${newItemAmount} ${itemName}(s).`);
+        M.reply(`Thank you for your purchase! You now have ${quantity} ${itemName}(s).`);
     },
 };
