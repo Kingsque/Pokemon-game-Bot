2c60315857f7074c2ca19a868c9808b461615cd8
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-30 12:37:15 +0530

    Update c.js

--- src/Commands/Dev/c.js
+++ src/Commands/Dev/c.js
@@ -15,7 +15,7 @@ module.exports = {
             tier = tier.replace('tier ', ''); // Remove 'tier' prefix
             const price = await client.utils.getRandomInt(10000, 100); // Assuming calculatePrice is a valid function
 
-            const message = `🎊 A new card has spawned 🎊\n\n🏷 *Name:* ${title}\n🪄 *Tier:* ${tier}\n💎 *Price:* ${price}\n\nUse *${client.config.prefix}collect* to get this card for yourself`;
+            const message = `🎊 A new card has spawned 🎊\n\n🏷 *Name:* ${title}\n🪄 *Tier:* ${tier}\n💎 *Price:* ${price}\n\nUse *${client.prefix}collect* to get this card for yourself`;
 
             if (image.endsWith('.gif')) {
                 const vid = await client.utils.gifToMp4(image); // Assuming gifToMp4 is a valid function
