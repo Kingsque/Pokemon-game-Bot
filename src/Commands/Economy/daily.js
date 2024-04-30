af5433e8dfa9e805917d3470c12d9b30f9b60186
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-30 21:38:30 +0530

    Update daily.js

--- src/Commands/Economy/daily.js
+++ src/Commands/Economy/daily.js
@@ -1,4 +1,4 @@
-const ms = require('parse-ms')
+const ms = require('parse-ms');
 
 module.exports = {
     name: 'daily',
@@ -8,20 +8,27 @@ module.exports = {
     react: "✅",
     description: 'Claims your daily rewards',
     async execute(client, arg, M) {
-      const dailyTimeout = 86400000;
-      const dailyAmount = 1000;
-      const userId = M.sender;
-      const economy = await client.econ.findOne({ userId });
-      const daily = economy.daily;
-      let message = '';
-      if (daily !== null && dailyTimeout - (Date.now() - daily) > 0) {
-        const dailyTime = ms(dailyTimeout - (Date.now() - daily));
-        message = `*You have already claimed your daily reward. You have to wait ${dailyTime.hours} hour(s) ${dailyTime.minutes} minute(s), ${dailyTime.seconds} second(s).*`;
-      } else {
-        message = `*You have claimed your daily reward 🎉: ${dailyAmount}.*`;
-        economy.wallet += dailyAmount;
-        await economy.save();
-      }
-      M.reply(message);
+        const dailyTimeout = 86400000;
+        const dailyAmount = 1000;
+        const userId = M.sender;
+        let message = '';
+
+        const economy = await client.econ.findOne({ userId });
+        if (!economy) {
+            message = "*You haven't set up your economy yet.*";
+        } else {
+            const daily = economy.lastDaily;
+            if (daily !== null && dailyTimeout - (Date.now() - daily) > 0) {
+                const dailyTime = ms(dailyTimeout - (Date.now() - daily));
+                message = `*You have already claimed your daily reward. You have to wait ${dailyTime.hours} hour(s) ${dailyTime.minutes} minute(s), ${dailyTime.seconds} second(s).*`;
+            } else {
+                message = `*You have claimed your daily reward 🎉: ${dailyAmount}.*`;
+                economy.wallet += dailyAmount;
+                economy.lastDaily = Date.now();
+                await economy.save();
+            }
+        }
+
+        M.reply(message);
     },
-  };
+};
