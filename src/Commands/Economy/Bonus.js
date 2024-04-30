2a6e2b5c710f28aff587939f1e75c064e089eb20
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-04-30 14:30:49 +0530

    Update Bonus.js

--- src/Commands/Economy/Bonus.js
+++ src/Commands/Economy/Bonus.js
@@ -11,30 +11,32 @@ module.exports = {
     usage: 'Use :bonus',
     description: 'Claims your bonus',
     async execute(client, arg, M) {
+        const userId = M.sender; // Fixed missing semicolon
+        const economy = await client.econ.findOne({ userId });
         const bonusTimeout = 31536000000; 
         const bonusAmount = 100000;
-        const bonus = await client.gem.get(`${M.sender}_bonus`);
         let text = '';
 
-        if (bonus !== null && bonusTimeout - (Date.now() - bonus) > 0) {
-            const bonusTime = ms(bonusTimeout - (Date.now() - bonus));
+        if (economy && economy.bonus !== null && bonusTimeout - (Date.now() - economy.bonus) > 0) { // Added check if economy exists
+            const bonusTime = ms(bonusTimeout - (Date.now() - economy.bonus));
             text += `*You have already claimed your bonus reward. You cannot claim it again.*`;
         } else {
             text += `*Welcome to our Aurora family! We are really happy to have you as our member. You have claimed your bonus reward 🎉: ${bonusAmount}.*`;
 
-            await client.gem.add(`${M.sender}_wallet`, bonusAmount);
-            await client.gem.set(`${M.sender}_bonus`, Date.now());
+            economy.gem += bonusAmount; // Fixed missing semicolon
+            economy.bonus = Date.now(); // Fixed incorrect capitalization of 'Date'
+            await economy.save();
         }
 
-      await client.sendMessage(
-          M.from,
-          {
-            image: { url: "https://i.ibb.co/tPhb428/Aurora.jpg" },
-            caption: text
-          },
-          {
-            quoted: M
-          }
+        await client.sendMessage(
+            M.from,
+            {
+                image: { url: "https://i.ibb.co/tPhb428/Aurora.jpg" },
+                caption: text
+            },
+            {
+                quoted: M
+            }
         );
     }
 };
