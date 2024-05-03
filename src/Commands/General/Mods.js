cd32f73d327cdcc63fba3f2914d6c57df0dbdea9
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2024-05-03 14:46:35 +0530

    Update Mods.js

--- src/Commands/General/Mods.js
+++ src/Commands/General/Mods.js
@@ -4,12 +4,12 @@ module.exports = {
   category: 'general',
   exp: 0,
   cool: 5,
-  react: "✅",
+  react: "🔬",
   usage: 'Use :mods',
   description: 'Get information about moderators',
   async execute(client, arg, M) {
     let mods = client.mods;
-    let mo = "*Aurora MODS*\n";
+    let mo = "*Moderator's 📡*\n";
     
     for (let i = 0; i < mods.length; i++) {
       const contact = await client.contact.getContact(mods[i], client);
