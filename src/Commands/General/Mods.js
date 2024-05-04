abd79c0074699eec4937c56e674cb1db2a29dcc0
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2024-05-04 16:36:11 +0530

    Update Mods.js

--- src/Commands/General/Mods.js
+++ src/Commands/General/Mods.js
@@ -15,7 +15,7 @@ module.exports = {
       const contact = await client.contact.getContact(mods[i], client);
       const pushname = contact.username?.whatsapp?.net.toString() || 'Unknown'; // Ensure pushname is always a string
       const tag = mods[i].split('@s.whatsapp.net')[0];
-      mo += `\n#${i + 1}) *Name:* ${pushname}\n*Contact:* http://wa.me/+${mods[i]}\ntag: ${tag}`;
+      mo += `\n#${i + 1}) ❄️ ${pushname}\n🎲  http://wa.me/+${mods[i]}\ntag: ${tag}`;
     }
     
     await client.sendMessage(
