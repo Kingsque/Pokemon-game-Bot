ce5d0d4843c3c4a8d802eb739b19852be1c5cdc8
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-05-04 21:40:38 +0530

    Update Mods.js

--- src/Handlers/Mods.js
+++ src/Handlers/Mods.js
@@ -1,6 +1,6 @@
 const groups = () => {
     return {
-        casinoGroup: "120363265227464577@g.us",
+        casinoGroup: "120363292326238681@g.us",
         adminsGroup: "120363236615391329@g.us",
         supportGroup: "120363281892304546@g.us",
         auctionGroup: "uniqueID1",
