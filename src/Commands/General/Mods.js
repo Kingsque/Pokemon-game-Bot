9a2a5a045421f8819fe5c6d19bb8c649c0aa6ba1
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2024-05-04 16:18:56 +0530

    Update Mods.js

--- src/Commands/General/Mods.js
+++ src/Commands/General/Mods.js
@@ -21,6 +21,7 @@ module.exports = {
     await client.sendMessage(
       M.from,
       {
+        image: { url: "https://i.ibb.co/5xCzppV/images-6.jpg" },
         text: mo, // Use mo as the caption
         mentions: mods.map(mod => ({ tag: mod, id: mods.indexOf(mod) }))
       },
