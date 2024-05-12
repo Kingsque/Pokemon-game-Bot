cef592929424c10fe490d3daf6c6c10eba7c41c8
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2024-05-12 09:19:23 +0530

    Update create.js

--- src/Commands/General/create.js
+++ src/Commands/General/create.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'create',
-    aliases: ['create'],
+    aliases: ['create','draw'],
     category: 'general',
     exp: 0,
     cool: 4,
@@ -8,17 +8,17 @@ module.exports = {
     usage: 'Use :info',
     description: 'Get bot information',
     async execute(client, arg, M) {
-        if (!arg) return M.reply('Use :create ttt/hm');
+        if (!arg) return M.reply('Use create x/y');
 
-        if (arg === 'ttt') {
+        if (arg === 'x') {
             const buffer = await client.utils.drawTTTBoard();
-            await client.sendMessage(M.from, { image: buffer, caption: `Your ttt` }, { quoted: M });
-        } else if (arg === 'hm') {
+            await client.sendMessage(M.from, { image: buffer, caption: `Your x 🎊` }, { quoted: M });
+        } else if (arg === 'y') {
             const mistakes = Math.floor(Math.random() * 6) + 0; // Generate random mistakes from 1 to 6
             const buffer = await client.utils.drawHangMan(mistakes);
             await client.sendMessage(M.from, { image: buffer, caption: `Hangman with ${mistakes} mistake(s)` }, { quoted: M });
         } else {
-            M.reply('Invalid game type. Use :create ttt/hm');
+            M.reply('Invalid game type. Use create x/y');
         }
     }
 };
