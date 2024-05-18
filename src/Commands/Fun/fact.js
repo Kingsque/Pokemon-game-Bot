aefc06ae84b9c4094233bb3cc7be1331c06f1f4a
Yugesh Singh <98589136+shizothetechie@users.noreply.github.com>
2024-05-18 17:16:40 +0530

    Update fact.js

--- src/Commands/Fun/fact.js
+++ src/Commands/Fun/fact.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { generateWAMessageFromContent, proto, generateWAMessage, areJidsSameUser } = require('@whiskeysockets/baileys');
 
 module.exports = {
     name: 'fact',
@@ -13,7 +14,45 @@ module.exports = {
         try {
             const response = await axios.get('https://nekos.life/api/v2/fact');
             const text = `Fact for you: ${response.data.fact}`;
-            M.reply(text);
+       let msg = generateWAMessageFromContent(m.chat, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: text
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "RedZeox"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "",
+            subtitle: "",
+            hasMediaAttachment: false
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\"Buttons Example\",\"id\":\"/owner\"}"
+              },
+              {
+                 "name": "cta_url",
+                 "buttonParamsJson": "{\"display_text\":\"url\",\"url\":\"https://www.google.com/search?q=Shizo+The+Techie\",\"merchant_url\":\"https://www.google.com/search?q=Shizo+The+Techie\"}"
+              }
+           ],
+          })
+        })
+    }
+  }
+}, {})
+
+ client.relayMessage(msg.key.remoteJid, msg.message, {
+  messageId: msg.key.id
+})
         } catch (err) {
             console.error('Error fetching fact:', err);
             M.reply('Error fetching fact. Please try again later.');
