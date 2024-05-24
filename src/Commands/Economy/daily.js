83387e6611f4e495cf80d74af66a66400f7d87eb
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2024-05-24 09:56:54 +0530

    Update daily.js

--- src/Commands/Economy/daily.js
+++ src/Commands/Economy/daily.js
@@ -28,6 +28,17 @@ module.exports = {
                 await economy.save();
             }
         }
+        await client.sendMessage(
+            M.from,
+            {
+                image: { url: "https://i.ibb.co/Ldd8bp7/1057308.jpg" },
+                caption: text
+            },
+            {
+                quoted: M
+            }
+        );
+        
        M.reply(message);
     },
 };
