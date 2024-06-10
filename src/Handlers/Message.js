7e73b88886031c0d5be899610a4fabed3d9f091e
uchihaShisui <127178249+Kingshisui00@users.noreply.github.com>
2024-06-10 12:21:37 +0530

    Update Message.js

--- src/Handlers/Message.js
+++ src/Handlers/Message.js
@@ -54,6 +54,8 @@ module.exports = MessageHandler = async (messages, client) => {
             }
         }
 
+        if ( body === 'test' || body === 'Test') return M.reply(`Everything is working fine ${M.pushName}`)
+      
         // Link handling code
         if (!isGroup && body.includes('chat.whatsapp.com')) {
             const senderInfo = M.pushName || sender;
