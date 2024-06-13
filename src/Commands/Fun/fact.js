29e0abdd273cb2dd680a2182ed1f53d6c386d65b
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-06-13 10:11:00 +0530

    Update fact.js

--- src/Commands/Fun/fact.js
+++ src/Commands/Fun/fact.js
@@ -1,4 +1,10 @@
 const axios = require('axios');
+const {
+    proto,
+    generateWAMessage,
+    areJidsSameUser,
+    generateWAMessageFromContent
+} = require('@shizoweb/baileys');
 
 module.exports = {
     name: 'fact',
@@ -14,6 +20,42 @@ module.exports = {
             const response = await axios.get('https://nekos.life/api/v2/fact');
             const text = `Fact for you: ${response.data.fact}`;
             M.reply(text);
+            
+  let msg = generateWAMessageFromContent(m.chat, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: `${text}`
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "© RedZeoX"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "",
+            subtitle: "",
+            hasMediaAttachment: false
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\"quick_reply\",\"id\":\"/#menu\"}"
+              }
+           ],
+          })
+        })
+    }
+  }
+}, {})
+
+await client.relayMessage(msg.key.remoteJid, msg.message, {
+  messageId: msg.key.id
+})
         } catch (err) {
             console.error('Error fetching fact:', err);
             M.reply('Error fetching fact. Please try again later.');
