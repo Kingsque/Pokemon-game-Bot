1f96ca9b07a4f97a4aedf9aaa4cbcdcf8fcb6c36
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-06-13 16:46:45 +0530

    Create Fact-listtest.js

---
+++ src/Commands/Fun/Fact-listtest.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+const {
+    proto,
+    generateWAMessage,
+    areJidsSameUser,
+    generateWAMessageFromContent
+} = require('@WhiskeySockets/baileys');
+
+module.exports = {
+    name: 'fact',
+    aliases: ['ft'],
+    category: 'fun',
+    exp: 5,
+    cool: 4,
+    react: "🍥",
+    usage: 'Use :fact',
+    description: 'Sends random facts',
+    async execute(client, arg, M) { 
+        try {
+            const response = await axios.get('https://nekos.life/api/v2/fact');
+            const text = `Fact for you: ${response.data.fact}`;
+            
+let msg = generateWAMessageFromContent(M.from, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: "Choose Valid Option"
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "© RedZeoX"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "",
+            subtitle: "",
+            hasMediaAttachment: false
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+            {
+                "name": "single_select",
+                "buttonParamsJson": "{\"title\":\"title\",\"sections\":[{\"title\":\"title\",\"highlight_label\":\"label\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
+              }
+           ],
+          })
+        })
+    }
+  }
+}, {})
+
+await client.relayMessage(msg.key.remoteJid, msg.message, {
+  messageId: msg.key.id
+})
+        } catch (err) {
+            console.error('Error fetching fact:', err);
+            M.reply('Error fetching fact. Please try again later.');
+        }
+    }
+};
