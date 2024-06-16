f69e53d072aa4509b836730032851fdd6c3bf11a
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2024-06-16 23:37:16 +0530

    Update truth_dare.js

--- src/Commands/Fun/truth_dare.js
+++ src/Commands/Fun/truth_dare.js
@@ -1,10 +1,5 @@
 const TD = require('better-tord');
-const {
-    proto,
-    generateWAMessage,
-    areJidsSameUser,
-    generateWAMessageFromContent
-} = require('@WhiskeySockets/baileys');
+const { shizobtn1, shizobtn1img, shizobtn1gif, shizobtn2 } = require('../../shizofunc.js')
 
 module.exports = {
     name: 'truth_dare',
@@ -16,53 +11,9 @@ module.exports = {
     usage: 'Use :td truth or dare',
     description: 'Gives you truth or dare.',
     async execute(client, arg, M) {
-        if (!arg) {
-        let msg = generateWAMessageFromContent(M.from, {
-  viewOnceMessage: {
-    message: {
-        "messageContextInfo": {
-          "deviceListMetadata": {},
-          "deviceListMetadataVersion": 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({
-            text: `choose from the below list:-`
-          }),
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: "𒉢 ꜱᴀʏ.ꜱᴄ֟፝ᴏᴛᴄʜ ⚡𐇻"
-          }),
-          header: proto.Message.InteractiveMessage.Header.create({
-            title: "",
-            subtitle: "",
-            hasMediaAttachment: false
-          }),
-          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons: [
-            {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"Truth\",\"id\":\"-td truth\"}"
-              },
-                {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"Dare\",\"id\":\"-td dare\"}"
-                }
-           ],
-          })
-        })
-    }
-  }
-}, {})
-
-await client.relayMessage(msg.key.remoteJid, msg.message, {
-  messageId: msg.key.id
-})
-        }
-        
+        if (!arg) return shizobtn2(client, M, 'choose from the below list:-', 'Truth', '-td truth', 'Dare', '-td dare', '𒉢 ꜱᴀʏ.ꜱᴄ֟፝ᴏᴛᴄʜ ⚡𐇻')
         const availableOptions = ['truth', 'dare'];
         const option = arg.trim().toLowerCase();
-
-        
-
         try {
             const result = option === 'truth' ? await TD.get_truth() : await TD.get_dare();
             M.reply(`Here's your ${option}: ${result}`);
