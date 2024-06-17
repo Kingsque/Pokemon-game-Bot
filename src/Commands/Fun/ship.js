a3d2963611d3df4659350c945128460248839df1
ꜱᴀʏ.ꜱᴄᴏᴛᴄʜ <111042925+HELLRYZEN@users.noreply.github.com>
2024-06-17 15:11:17 +0530

    Update ship.js

--- src/Commands/Fun/ship.js
+++ src/Commands/Fun/ship.js
@@ -24,7 +24,7 @@ module.exports = {
                 image = await client.utils.getBuffer(await client.profilePictureUrl(user, 'image'))
             } catch {
                 image = await client.utils.getBuffer(
-                    'https://icon2.cleanpng.com/20180703/lzk/kisspng-computer-icons-error-clip-art-checklist-5b3c119612f6e8.7675651415306633180777.jpg'
+                    'https://i.ibb.co/vB8mqt1/riveryicons-20240605-0003.jpg'
                 )
             }
             shipArray.push({ name, image })
