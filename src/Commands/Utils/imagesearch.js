bdd18d1b1746231f861ec3594786f84d130da4bd
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2024-06-19 11:29:25 +0530

    Update imagesearch.js

--- src/Commands/Utils/imagesearch.js
+++ src/Commands/Utils/imagesearch.js
@@ -1,11 +1,11 @@
-/* const google = require('googlethis')
+const google = require('googlethis')
 
 module.exports = {
     name: 'imagesearch',
-    aliases: ['imgs'],
+    aliases: ['imgs','getimg'],
     category: 'utils',
     exp: 7,
-    react: "✅",
+    react: "🖼️",
     description: 'Searches image from google.com',
     async execute(client, arg, M) {
         if (!arg) return M.reply('Sorry you did not give any search term!')
@@ -20,54 +20,4 @@ module.exports = {
             caption: 'Here you go'
         })
     }
-} */
-
-
-
-const fetch = require('node-fetch');
-
-const apiKey = 'AIzaSyD8yD-Bx4Pb43lKoOKl-mLOdujQWV_wIAs';
-const cx = 'e47f554165f6a4137';
-
-module.exports = {
-    name: 'getimg',
-    aliases: ['searchimg'],
-    category: 'utils',
-    exp: 1,
-    react: "🎶",
-    usage: 'Use :getimg <context>',
-    description: 'Searches image from google.com',
-    cool: 4,
-    async execute(client, arg, M) {
-        try {
-            if (!arg || arg.length === 0) {
-                return M.reply('Sorry, you did not provide any search term!');
-            }
-
-            const searchTerm = arg.join(' ');
-            const url = `https://www.googleapis.com/customsearch/v1?q=${searchTerm}&searchType=image&key=${apiKey}&cx=${cx}`;
-
-            const response = await fetch(url);
-            const data = await response.json();
-
-            // Check if the response contains valid data
-            if (!data.items || data.items.length === 0) {
-                return M.reply('Could not find any images for the searched term.');
-            }
-
-            // Get the URL of the first image
-            const imageUrl = data.items[0].link;
-
-            M.reply('Searching for the image from the web...');
-
-            // Send the image URL as a message
-            await client.sendMessage(M.from, {
-                image: { url: imageUrl },
-                caption: `Here is the result for your searched image (${searchTerm})`
-            });
-        } catch (error) {
-            console.error('Error searching for images:', error);
-            M.reply('An error occurred while searching for images.');
-        }
-    }
-};
+}
