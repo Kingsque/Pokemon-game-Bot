20117973ef926910aa35f45ae93f254419d0b41c
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2024-06-24 17:59:14 +0530

    Update ytaudio.js

--- src/Commands/Media/ytaudio.js
+++ src/Commands/Media/ytaudio.js
@@ -1,52 +1,43 @@
-const YT = require('../../lib/YT');
-const yts = require('yt-search');
-
 module.exports = {
     name: 'ytaudio',
-    aliases: ['yta', 'play'],
+    aliases: ['yta'],
     category: 'media',
-    react: "🎶",
+    exp: 5,
+    cool: 4,
+    react: "✅",
     usage: 'Use :ytaudio <song_link>',
     description: 'Downloads given YouTube Video and sends it as Audio',
     async execute(client, arg, M) {
-        try {
-            const link = async (term) => {
-                const { videos } = await yts(term.trim());
-                if (!videos || !videos.length) return null;
-                return videos[0].url;
-            };
-
-            if (!arg) return M.reply('Please use this command with a valid YouTube link');
-
-            const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/;
-            const term = validPathDomains.test(arg) ? arg.trim() : await link(arg);
-            if (!term) return M.reply('Please use this command with a valid YouTube content link');
-
-            if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid YouTube link');
+	@@ -28,23 +26,7 @@ module.exports = {
 
             const { videoDetails } = await YT.getInfo(term);
 
-            M.reply(`Downloading: ${videoDetails.title}`);
+            M.reply('Downloading has started, please wait...');
 
-            // Checking if the video is longer than 30 minutes
-            if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('Cannot download audio longer than 30 minutes');
+            let text = `*Title:* ${videoDetails.title} | *Type:* Audio | *From:* ${videoDetails.ownerChannelName}`;
 
-            // Downloading and sending the audio
-            const audioBuffer = await YT.getBuffer(term, 'audio');
-            await client.sendMessage(
+            // Sending thumbnail and video details
+            client.sendMessage(
                 M.from,
                 {
-                    document: audioBuffer,
-                    mimetype: 'audio/mpeg',
-                    fileName: `${videoDetails.title}.mp3`
+                    image: {
+                        url: `https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`
+                    },
+                    caption: text
                 },
                 {
                     quoted: M
                 }
             );
-        } catch (error) {
-            console.error(error);
-            M.reply('An error occurred while downloading the YouTube video audio.');
-        }
-    }
-};
+
+            // Checking if the video is longer than 30 minutes
+            if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('Cannot download audio longer than 30 minutes');
+	@@ -54,7 +36,7 @@ module.exports = {
+            await client.sendMessage(
+                M.from,
+                {
+                    audio: audioBuffer,
+                    mimetype: 'audio/mpeg',
+                    fileName: `${videoDetails.title}.mp3`
+                },
+	
