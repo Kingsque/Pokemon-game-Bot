dd55ed0d69b5de69301db5e95fe01aeefd0fad5b
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2024-06-24 18:03:53 +0530

    Update ytaudio.js

--- src/Commands/Media/ytaudio.js
+++ src/Commands/Media/ytaudio.js
@@ -1,3 +1,6 @@
+const YT = require('../../lib/YT');
+const yts = require('yt-search');
+
 module.exports = {
     name: 'ytaudio',
     aliases: ['yta'],
@@ -8,7 +11,20 @@ module.exports = {
     usage: 'Use :ytaudio <song_link>',
     description: 'Downloads given YouTube Video and sends it as Audio',
     async execute(client, arg, M) {
-	@@ -28,23 +26,7 @@ module.exports = {
+        try {
+            const link = async (term) => {
+                const { videos } = await yts(term.trim());
+                if (!videos || !videos.length) return null;
+                return videos[0].url;
+            };
+
+            if (!arg) return M.reply('Please use this command with a valid YouTube link');
+
+            const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/;
+            const term = validPathDomains.test(arg) ? arg.trim() : await link(arg);
+            if (!term) return M.reply('Please use this command with a valid YouTube content link');
+
+            if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid YouTube link');
 
             const { videoDetails } = await YT.getInfo(term);
 
@@ -32,7 +48,9 @@ module.exports = {
 
             // Checking if the video is longer than 30 minutes
             if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('Cannot download audio longer than 30 minutes');
-	@@ -54,7 +36,7 @@ module.exports = {
+
+            // Downloading and sending the audio
+            const audioBuffer = await YT.getBuffer(term, 'audio');
             await client.sendMessage(
                 M.from,
                 {
@@ -40,4 +58,13 @@ module.exports = {
                     mimetype: 'audio/mpeg',
                     fileName: `${videoDetails.title}.mp3`
                 },
-	
+                {
+                    quoted: M
+                }
+            );
+        } catch (error) {
+            console.error(error);
+            M.reply('An error occurred while downloading the YouTube video audio.');
+        }
+    }
+};
