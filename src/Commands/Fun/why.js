0d0dd984401dc78b21cd8430ea0ced712ed86261
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2024-06-27 13:38:29 +0530

    Update why.js

--- src/Commands/Fun/why.js
+++ src/Commands/Fun/why.js
@@ -1,22 +1,63 @@
-const axios = require('axios')
+const axios = require('axios');
+const {
+    proto,
+    generateWAMessage,
+    areJidsSameUser,
+    generateWAMessageFromContent
+} = require('@WhiskeySockets/baileys');
 
 module.exports = {
-    name: "why",
-    aliases: ["Why"],
-    description: "Gives you random whys.",
-    category: "fun",
-    react:"🖇️",
+    name: 'Why',
+    aliases: ['why'],
+    category: 'fun',
+    exp: 5,
+    cool: 4,
+    react: "⁉️",
+    usage: 'Use :fact',
+    description: 'Sends random facts',
+    async execute(client, arg, M) { 
+        try {
+            const response = await axios.get('https://nekos.life/api/v2/why');
+            const text = `📝 *Question:-*\n> ${response.data.why}`;
+            
+  let msg = generateWAMessageFromContent(M.from, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: `${text}`
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "𒉢 ꜱᴀʏ.ꜱᴄ֟፝ᴏᴛᴄʜ ⚡𐇻"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "",
+            subtitle: "",
+            hasMediaAttachment: false
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\"Next One 🎐\",\"id\":\"-why\"}"
+              }
+           ],
+          })
+        })
+    }
+  }
+}, {})
 
- async execute(client, arg, M) {
- await axios
-.get(`https://nekos.life/api/v2/why`)
-.then((response) => {
-  // console.log(response);
-  const text = `📝 *Question:-*\n> ${response.data.why}`;
-  M.reply(text);
+await client.relayMessage(msg.key.remoteJid, msg.message, {
+  messageId: msg.key.id
 })
-.catch((err) => {
-  M.reply(`🔍 Error: ${err}`);
-});
-}
-}
+        } catch (err) {
+            console.error('Error fetching Why:', err);
+            M.reply('Error fetching Why. Please try again later.');
+        }
+    }
+};
