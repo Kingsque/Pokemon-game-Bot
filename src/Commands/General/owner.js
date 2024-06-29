b16cb39210f7859749542ed8f8e68e65e94b5490
ꜱᴀʏ.ꜱᴄᴏᴛᴄʜ <111042925+HELLRYZEN@users.noreply.github.com>
2024-06-29 17:23:47 +0530

    Update owner.js

--- src/Commands/General/owner.js
+++ src/Commands/General/owner.js
@@ -4,12 +4,13 @@ module.exports = {
     name: 'owner',
     aliases: ['own'],
     category: 'general',
-    react: "📢",
+    react: "💬",
     description: 'Get information bot information',
     async execute(client, arg, M) {
         let number = '919529426293@s.whatsapp.net'
         const owner = number
-        let text = `*🎍Owner:-* @${owner.split('@')[0]}\n*I'm The Owner Of This Bot..!`
+        let text = `*🎍ᴏᴡɴᴇʀ:-* [ ${owner.split('@')[0]} ]\n*ʜᴇʟʟᴏ ᴛʜᴇʀᴇ ɪ'ᴍ ʀᴇᴅᴢᴇᴏꭗ ᴛʜᴇ ᴏᴡɴᴇʀ ᴍᴀɪ ꜱᴀᴋᴜʀᴀᴊɪᴍᴀ ᴍᴅ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴜꜱɪɴɢ ᴛʜɪꜱ ᴜᴘᴘᴇʀ ʟᴇᴠᴇʟ ʙᴏᴛ..!
+ɪᴛ'ꜱ ᴀ ᴘʀɪᴠᴀᴛᴇ ʀᴇᴘᴏ ꜱᴏʀʀʏ 💬*`
         return shizobtn1img(client, M.from, text, "https://telegra.ph/file/fe7d26d07ca4a88657159.jpg", "Manual 👋", "-help", "𒉢 ꜱᴀʏ.ꜱᴄ֟፝ᴏᴛᴄʜ ⚡𐇻")
        
     }
