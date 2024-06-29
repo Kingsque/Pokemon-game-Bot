b173cb83e64eb69eecf95d35a28f88ec878363e0
ꜱᴀʏ.ꜱᴄᴏᴛᴄʜ <111042925+HELLRYZEN@users.noreply.github.com>
2024-06-29 17:33:02 +0530

    Update owner.js

--- src/Commands/General/owner.js
+++ src/Commands/General/owner.js
@@ -9,7 +9,7 @@ module.exports = {
     async execute(client, arg, M) {
         let number = '919529426293@s.whatsapp.net'
         const owner = number
-        let text = `*🎍ᴏᴡɴᴇʀ:-* [ ${owner.split('@')[0]} ]\n*ʜᴇʟʟᴏ ᴛʜᴇʀᴇ ɪ'ᴍ ʀᴇᴅᴢᴇᴏꭗ ᴛʜᴇ ᴏᴡɴᴇʀ ᴍᴀɪ ꜱᴀᴋᴜʀᴀᴊɪᴍᴀ ᴍᴅ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴜꜱɪɴɢ ᴛʜɪꜱ ᴜᴘᴘᴇʀ ʟᴇᴠᴇʟ ʙᴏᴛ..! ɪᴛ'ꜱ ᴀ ᴘʀɪᴠᴀᴛᴇ ʀᴇᴘᴏ ꜱᴏʀʀʏ 💬*`
+        let text = `*🎍ᴏᴡɴᴇʀ:-* [ ${owner.split('@')[0]} ]\n*ʜᴇʟʟᴏ ᴛʜᴇʀᴇ ɪ'ᴍ ʀᴇᴅᴢᴇᴏꭗ ᴛʜᴇ ᴏᴡɴᴇʀ ᴏғ ᴍᴀɪ ꜱᴀᴋᴜʀᴀᴊɪᴍᴀ ᴍᴅ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴜꜱɪɴɢ ᴛʜɪꜱ ᴜᴘᴘᴇʀ ʟᴇᴠᴇʟ ʙᴏᴛ..! ɪᴛ'ꜱ ᴀ ᴘʀɪᴠᴀᴛᴇ ʀᴇᴘᴏ ꜱᴏʀʀʏ 💬*`
         return shizobtn1img(client, M.from, text, "https://telegra.ph/file/fe7d26d07ca4a88657159.jpg", "Manual 👋", "-help", "𒉢 ꜱᴀʏ.ꜱᴄ֟፝ᴏᴛᴄʜ ⚡𐇻")
        
     }
